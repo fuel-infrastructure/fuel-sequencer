012dc7e63ea331275568dc789c8e7412af136c3a
migueldingli1997 <migueldingli97@gmail.com>
2024-02-28 22:37:39 +0000

    Refactor
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- x/bridge/types/genesis.go
+++ x/bridge/types/genesis.go
@@ -8,9 +8,9 @@ const DefaultIndex uint64 = 1
 // DefaultGenesis returns the default genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
+		Params:          DefaultParams(),
 		SupplyDeltaInfo: nil,
 		// this line is used by starport scaffolding # genesis/types/default
-		Params: DefaultParams(),
 	}
 }
 
