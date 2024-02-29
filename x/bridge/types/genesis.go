b356f2829cec90e422f693db9ba9b43700b50e83
migueldingli1997 <migueldingli97@gmail.com>
2024-02-29 09:57:22 +0000

    Set proper default genesis supply delta info
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- x/bridge/types/genesis.go
+++ x/bridge/types/genesis.go
@@ -1,5 +1,9 @@
 package types
 
+import (
+	"cosmossdk.io/math"
+)
+
 // this line is used by starport scaffolding # genesis/types/import
 
 // DefaultIndex is the default global index
@@ -8,8 +12,11 @@ const DefaultIndex uint64 = 1
 // DefaultGenesis returns the default genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
-		Params:          DefaultParams(),
-		SupplyDeltaInfo: nil,
+		Params: DefaultParams(),
+		SupplyDeltaInfo: &SupplyDeltaInfo{
+			Mint: math.ZeroInt(),
+			Burn: math.ZeroInt(),
+		},
 		// this line is used by starport scaffolding # genesis/types/default
 	}
 }
