bfbf0fde543e29e4f0378b1f8ec4aee9e19a50b6
migueldingli1997 <migueldingli97@gmail.com>
2024-02-29 09:55:40 +0000

    Revert supply delta tracking
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- utils/run_proposal_cancellation.sh
+++
@@ -1,9 +0,0 @@
-PROPOSAL_ID=1
-
-fuelsequencerd tx gov submit-proposal utils/proposal_community_spend.json --from alice --home ./data/fuelsequencer --chain-id fuelsequencer-1 -y
-sleep 1
-fuelsequencerd q gov proposal $PROPOSAL_ID
-
-fuelsequencerd tx gov cancel-proposal $PROPOSAL_ID --from alice --home ./data/fuelsequencer --chain-id fuelsequencer-1 -y
-sleep 1
-fuelsequencerd q bridge show-supply-delta-info
