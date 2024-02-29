95c686a353885f50ed84cf7eef856741c3054459
migueldingli1997 <migueldingli97@gmail.com>
2024-02-29 13:53:42 +0000

    Minor changes
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- x/sequencing/types/codec.go
+++ x/sequencing/types/codec.go
@@ -9,12 +9,10 @@ import (
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgUpdateParams{},
 		&MsgPostBlob{},
 	)
 	// this line is used by starport scaffolding # 3
 
-	registry.RegisterImplementations((*sdk.Msg)(nil),
-		&MsgUpdateParams{},
-	)
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
