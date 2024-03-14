5e6269f42ffde8094aa375f51e7ec6d83e20b2aa
migueldingli1997 <migueldingli97@gmail.com>
2024-03-14 17:09:12 +0000

    Add deposit from Ethereum function
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- x/bridge/types/expected_keepers.go
+++ x/bridge/types/expected_keepers.go
@@ -18,7 +18,9 @@ type AccountKeeper interface {
 type BankKeeper interface {
 	SpendableCoins(context.Context, sdk.AccAddress) sdk.Coins
 	GetSupply(ctx context.Context, denom string) sdk.Coin
+	SendCoinsFromModuleToAccount(ctx context.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromAccountToModule(ctx context.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	MintCoins(ctx context.Context, moduleName string, amt sdk.Coins) error
 	BurnCoins(ctx context.Context, moduleName string, amt sdk.Coins) error
 	GetAllBalances(ctx context.Context, addr sdk.AccAddress) sdk.Coins
 	// Methods imported from bank should be defined here
