1bf08093ceaa4254e3668cea7220eb4b473a676a
migueldingli1997 <migueldingli97@gmail.com>
2024-03-15 16:22:17 +0000

    Move account validators to a utils file
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- x/bridge/keeper/accounts_test.go
+++ x/bridge/keeper/accounts_test.go
@@ -16,10 +16,6 @@ const (
 	token = "token"
 )
 
-type (
-	accountValidator func(acc sdk.AccountI) bool
-)
-
 func (s *KeeperTestSuite) TestGetSequencerAccountFromEthereumAddress() {
 
 	// ethAddr1Str -> seqAddr1Str
@@ -49,32 +45,6 @@ func (s *KeeperTestSuite) TestGetSequencerAccountFromEthereumAddress() {
 	token150 := sdk.NewCoins(sdk.NewInt64Coin(token, 150))
 	token100 := sdk.NewCoins(sdk.NewInt64Coin(token, 100))
 	token50 := sdk.NewCoins(sdk.NewInt64Coin(token, 50))
-
-	// matchesBaseAcc asserts that the account is a base account and matches the supplied account.
-	matchesBaseAcc := func(baseAcc *authtypes.BaseAccount) accountValidator {
-		return func(acc sdk.AccountI) bool {
-			bAcc, ok := acc.(*authtypes.BaseAccount)
-			return ok &&
-				(bAcc.GetAddress().Equals(baseAcc.GetAddress())) &&
-				((bAcc.PubKey == nil && baseAcc.PubKey == nil) || (bAcc.GetPubKey().Equals(baseAcc.GetPubKey()))) &&
-				(bAcc.GetAccountNumber() == baseAcc.GetAccountNumber()) &&
-				(bAcc.GetSequence() == baseAcc.GetSequence())
-		}
-	}
-
-	// matchesContinuousVestingAccount asserts that the account is a vesting account and matches the supplied account.
-	matchesContinuousVestingAccount := func(vestingAcc *vestingtypes.ContinuousVestingAccount) accountValidator {
-		return func(acc sdk.AccountI) bool {
-			vAcc, ok := acc.(*vestingtypes.ContinuousVestingAccount)
-			return ok &&
-				(vAcc.StartTime == vestingAcc.StartTime) &&
-				(vAcc.OriginalVesting.Equal(vestingAcc.OriginalVesting)) &&
-				(vAcc.DelegatedFree.Equal(vestingAcc.DelegatedFree)) &&
-				(vAcc.DelegatedVesting.Equal(vestingAcc.DelegatedVesting)) &&
-				(vAcc.EndTime == vestingAcc.EndTime) &&
-				matchesBaseAcc(vestingAcc.BaseAccount)(vAcc.BaseAccount)
-		}
-	}
 
 	type fnArgs struct {
 		ethAddress      string
