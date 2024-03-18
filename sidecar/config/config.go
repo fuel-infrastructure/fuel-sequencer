c5126c39dd1e81089af88a8ee3526406270ebb76
migueldingli1997 <migueldingli97@gmail.com>
2024-03-18 08:21:46 +0000

    Revert "Default Sidecar Enabled to true"
    This reverts commit 592df4537f9c936c51e95033b84517163f3a3008.

--- sidecar/config/config.go
+++ sidecar/config/config.go
@@ -15,7 +15,7 @@ const (
 	FlagSidecarAddress = "sidecar.address"
 	FlagSidecarTimeout = "sidecar.timeout"
 
-	DefaultSidecarEnabled = true
+	DefaultSidecarEnabled = false
 	DefaultSidecarAddress = "localhost:8080"
 	DefaultSidecarTimeout = time.Second * 5
 )
