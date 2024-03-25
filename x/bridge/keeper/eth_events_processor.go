4c23b108fbbd57d178642b30938caed175b4f89d
VitalyV1337 <vitalyvolozhinov@gmail.com>
2024-03-26 00:14:27 +0100

    Improving processSendToSequencerEvent
    Signed-off-by: VitalyV1337 <vitalyvolozhinov@gmail.com>

--- x/bridge/keeper/eth_events_processor.go
+++ x/bridge/keeper/eth_events_processor.go
@@ -1,7 +1,6 @@
 package keeper
 
 import (
-	"fmt"
 	"strings"
 	"time"
 
@@ -27,6 +26,7 @@ func (k Keeper) ProcessEthereumEvents(ctx sdk.Context) {
 
 	// Get the params as they are needed for the denom.
 	params := k.GetParams(ctx)
+	supplyDelta := k.MustGetSupplyDeltaInfo(ctx)
 
 	// Otherwise we being processing these events according to event type.
 	for _, event := range ethEventsTx.Events {
@@ -42,74 +42,80 @@ func (k Keeper) ProcessEthereumEvents(ctx sdk.Context) {
 
 		switch pe := parsedEvent.(type) {
 		case *sidecartypes.SendToSequencerEvent:
-			err = k.processSendToSequencerEvent(ctx, pe, params)
+			k.processSendToSequencerEvent(ctx, pe, params, &supplyDelta)
 		case *sidecartypes.AuthorizeEvent:
 			continue
 		default:
 			continue
 		}
 
-		fmt.Println(err)
 	}
+
+	// Set the supply delta info into state.
+	k.SetSupplyDeltaInfo(ctx, supplyDelta)
 }
 
 // processSendToSequencerEvent processes the send to sequencer events queried from the sidecar.
 // Deposit message cannot fail. If a failure can occur, we should consider minting the tokens
-// anyways and storing them in the governance address.
+// anyways and storing them in the governance address. The only time it can fail is if
+// we cannot parse the `Amount` of tokens as we won't know how many tokens have been processed.
 func (k Keeper) processSendToSequencerEvent(
 	ctx sdk.Context,
 	sendEvent *sidecartypes.SendToSequencerEvent,
 	params types.Params,
-) error {
+	supplyDeltaInfo *types.SupplyDeltaInfo,
+) {
 
 	// TODO: Check that the contract address matches the stored proxy contract address.
 
 	// Validate the send to sequencer event.
 	if err := sendEvent.ValidateBasic(); err != nil {
-		return err
+		k.Logger().Error("Bridge EndBlock: could not validate event", "err", err)
 	}
 
 	// Parse the data accordingly.
 	amount, success := sdkmath.NewIntFromString(sendEvent.Amount)
 	if !success {
-		return fmt.Errorf("failed to process send to sequencer amount to int from string")
+		k.Logger().Error("Bridge EndBlock: could not unmarshal amount to int from string")
 	}
 	totalCoins := sdk.NewCoins(sdk.NewCoin(params.BridgeDenom, amount))
 
 	// Parse the vesting duration
 	vesting, err := time.ParseDuration(sendEvent.Duration)
 	if err != nil {
-		return fmt.Errorf("failed to process send to sequencer duration from string: %s", err)
+		k.Logger().Error("Bridge EndBlock: failed to process send to sequencer duration from string", "err", err)
 	}
 
 	// If a `To` address was not specified send tokens to the `From` Ethereum Address.
 	if len(strings.TrimSpace(sendEvent.To)) == 0 {
 		err := k.depositFromEthereum(ctx, sendEvent.From, vesting, totalCoins)
 		if err != nil {
-			return err
+			k.Logger().Error("Bridge EndBlock: failed to deposit from ethereum", "err", err)
+		}
+	} else {
+		// Otherwise process the To from a string to an AccAddress type.
+		sequencerAddr, err := sdk.AccAddressFromBech32(sendEvent.To)
+		if err != nil {
+			k.Logger().Error("Bridge EndBlock: to is not a valid Bech32 address", "err", err)
 		}
 
-		return nil
+		// TODO Apply logic based on whether the Ethereum sender is the owner of the recipient address
+
+		// Otherwise mint and send the coins to the specified user.
+		err = k.bankKeeper.MintCoins(ctx, types.ModuleName, totalCoins)
+		if err != nil {
+			k.Logger().Error("Bridge EndBlock: failed to mint tokens to module", "err", err)
+		}
+
+		err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, sequencerAddr, totalCoins)
+		if err != nil {
+			k.Logger().Error("Bridge EndBlock: failed to send tokens from module to account", "err", err)
+		}
 	}
 
-	// Otherwise process the To from a string to an AccAddress type.
-	sequencerAddr, err := sdk.AccAddressFromBech32(sendEvent.To)
-	if err != nil {
-		return fmt.Errorf("to is not a valid Bech32 address: %w", err)
-	}
+	// Apply negative offset to supply delta offset
+	supplyDeltaInfo.Offset = supplyDeltaInfo.Offset.Sub(amount)
 
-	// TODO Apply logic based on whether the Ethereum sender is the owner of the recipient address
-
-	// Otherwise mint and send the coins to the specified user.
-	err = k.bankKeeper.MintCoins(ctx, types.ModuleName, totalCoins)
-	if err != nil {
-		return err
-	}
-
-	err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, sequencerAddr, totalCoins)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	// TODO do we store the supplyDeltaInfo or do we just keep adding the offset?
+	return
 }
