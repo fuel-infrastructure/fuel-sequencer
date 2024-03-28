0c4ba2d90ae03cfc758f5dc8092591f7cefb0038
Miguel Dingli <migueldingli97@gmail.com>
2024-03-28 16:09:09 +0000

    Align accounts implementation with updated spec (#76)
    * Prevent multiple vesting duration overrides
    
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>
    
    * Refactor and add extra test case
    
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>
    
    * Update PR template to add E2E tests step
    
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>
    
    * Add account owner TODOs
    
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>
    
    * Allow deposits to recipient addresses not owned by the Ethereum address
    
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>
    
    * Add account owner to EthOwned accounts
    
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>
    
    * Remove TODO
    
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>
    
    * Change var to const
    
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>
    
    * Simplify accounts logic
    
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>
    
    * Add more testing
    
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>
    
    * Add more test cases
    
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>
    
    * Update test-contracts
    
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>
    
    * Remove unused depositFromEthereum
    
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>
    
    * Update x/bridge/keeper/accounts_test.go
    
    Co-authored-by: VitalyV <60775046+VitalyV1337@users.noreply.github.com>
    
    * Update .github/PULL_REQUEST_TEMPLATE.md
    
    Co-authored-by: dillu24 <dylangalea96@gmail.com>
    
    * Update x/bridge/types/params.go
    
    Co-authored-by: dillu24 <dylangalea96@gmail.com>
    
    ---------
    
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>
    Co-authored-by: VitalyV <60775046+VitalyV1337@users.noreply.github.com>
    Co-authored-by: dillu24 <dylangalea96@gmail.com>

--- x/bridge/types/codec.go
+++ x/bridge/types/codec.go
@@ -4,10 +4,19 @@ import (
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	// this line is used by starport scaffolding # 1
 )
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
+	registry.RegisterImplementations((*sdk.AccountI)(nil),
+		&EthOwnedBaseAccount{},
+		&EthOwnedContinuousVestingAccount{},
+	)
+	registry.RegisterImplementations((*authtypes.GenesisAccount)(nil),
+		&EthOwnedBaseAccount{},
+		&EthOwnedContinuousVestingAccount{},
+	)
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgUpdateParams{},
 		&MsgSupplyDelta{},
--- x/bridge/types/params.go
+++ x/bridge/types/params.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"time"
+
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 )
 
@@ -10,6 +12,9 @@ const (
 	// AllowAllAuthorizeMessages can be used if we want to allow
 	// all messages instead of specifying all of them one-by-one.
 	AllowAllAuthorizeMessages = "*"
+
+	// vestingStartTimeDelay is a constant period of time during which tokens are completely locked.
+	vestingStartTimeDelay = time.Hour * 24 * 365
 )
 
 // ParamKeyTable the param key table for launch module
@@ -53,4 +58,25 @@ func (p Params) Validate() error {
 	// TODO: validate SupplyDeltaPeriod
 
 	return nil
+}
+
+// VestingTimesFromVestingDuration returns the vesting start and end time based on the VestingStartTime parameter, the
+// vestingStartTimeDelay, and a specified vesting duration, which must be greater than vestingStartTimeDelay since the
+// vesting duration is included in the vestingStartTimeDelay.
+//
+// Example: for a VestingStartTime set to 2024 and a vesting duration of 2 years:
+// - Actual vesting start time: 2024 + vestingStartTimeDelay = 2025
+// - Actual vesting end time: 2024 + vesting duration = 2026
+func (p Params) VestingTimesFromVestingDuration(duration time.Duration) (time.Time, time.Time, error) {
+
+	if duration <= vestingStartTimeDelay {
+		return time.Time{}, time.Time{}, ErrInvalidVestingDuration.Wrapf(
+			"duration must be greater than vesting start time delay, got %s <= %s",
+			duration, vestingStartTimeDelay,
+		)
+	}
+
+	vestingStartTime := p.VestingStartTime.Add(vestingStartTimeDelay)
+	vestingEndTime := p.VestingStartTime.Add(duration)
+	return vestingStartTime, vestingEndTime, nil
 }
