3d59ede826d2e2f5948c4878105f5d506f6b192f
migueldingli1997 <migueldingli97@gmail.com>
2024-05-03 11:21:41 +0200

    Fix TestAuthorizedTransactions_StakingOperations
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- e2e/testsuite/query_bridge.go
+++ e2e/testsuite/query_bridge.go
@@ -24,27 +24,53 @@ func (s *E2ETestSuite) QueryLastEthereumBlockSynced(ctx context.Context) uint64 
 }
 
 func (s *E2ETestSuite) PollForEthereumEventIndexOffset(
-	ctx context.Context, deltaBlocks uint64, ethereumEventIndexOffset uint64,
+	ctx context.Context, deltaBlocks uint64, offset uint64,
 ) {
 	h, err := s.Chain.FuelSequencerHeight(ctx)
 	s.Require().NoError(err)
 
-	s.T().Log(fmt.Sprintf("Polling for Ethereum event index offset %d", ethereumEventIndexOffset))
+	s.T().Log(fmt.Sprintf("Polling for Ethereum event index offset %d", offset))
 
 	doPoll := func(ctx context.Context, height uint64) (any, error) {
-		offset, err := s.Chain.grpcClients.BridgeQueryClient.EthereumEventIndexOffset(ctx,
+		resp, err := s.Chain.grpcClients.BridgeQueryClient.EthereumEventIndexOffset(ctx,
 			&bridgetypes.QueryGetEthereumEventIndexOffsetRequest{},
 		)
 		if err != nil {
 			return nil, err
 		}
-		if offset.Offset != ethereumEventIndexOffset {
-			return nil, fmt.Errorf("offset (%d) does not match expected: (%d)", offset.Offset, ethereumEventIndexOffset)
+		if resp.Offset != offset {
+			return nil, fmt.Errorf("offset (%d) does not match expected: (%d)", resp.Offset, offset)
 		}
 		return nil, nil
 	}
 
 	bp := BlockPoller[any]{CurrentHeight: s.Chain.FuelSequencerHeight, PollFunc: doPoll}
 	_, err = bp.DoPoll(ctx, h, h+deltaBlocks)
-	s.Require().NoError(err, "exact offset not found in expected number of blocks")
+	s.Require().NoError(err, fmt.Errorf("exact offset (%d) not found in expected number of blocks", offset))
+}
+
+func (s *E2ETestSuite) PollForLastEthereumBlockSynced(
+	ctx context.Context, deltaBlocks uint64, block uint64,
+) {
+	h, err := s.Chain.FuelSequencerHeight(ctx)
+	s.Require().NoError(err)
+
+	s.T().Log(fmt.Sprintf("Polling for last Ethereum block synced %d", block))
+
+	doPoll := func(ctx context.Context, height uint64) (any, error) {
+		resp, err := s.Chain.grpcClients.BridgeQueryClient.LastEthereumBlockSynced(ctx,
+			&bridgetypes.QueryGetLastEthereumBlockSyncedRequest{},
+		)
+		if err != nil {
+			return nil, err
+		}
+		if resp.Block != block {
+			return nil, fmt.Errorf("last block synced (%d) does not match expected: (%d)", resp.Block, block)
+		}
+		return nil, nil
+	}
+
+	bp := BlockPoller[any]{CurrentHeight: s.Chain.FuelSequencerHeight, PollFunc: doPoll}
+	_, err = bp.DoPoll(ctx, h, h+deltaBlocks)
+	s.Require().NoError(err, fmt.Errorf("exact last block synced %d not found in expected number of blocks", block))
 }
--- e2e/testsuite/query_staking.go
+++ e2e/testsuite/query_staking.go
@@ -26,7 +26,8 @@ func (s *E2ETestSuite) QueryDelegation(
 	return res.DelegationResponse
 }
 
-// PollForDelegationBalance polls until the delegation balance matches
+// PollForDelegationBalance polls until the delegation balance matches.
+// Note: to poll for no delegation, use PollForNoDelegation instead.
 func (s *E2ETestSuite) PollForDelegationBalance(
 	ctx context.Context, deltaBlocks uint64, delegatorAddress, validatorAddress string, balance sdk.Coin,
 ) {
@@ -56,4 +57,39 @@ func (s *E2ETestSuite) PollForDelegationBalance(
 	bp := BlockPoller[any]{CurrentHeight: s.Chain.FuelSequencerHeight, PollFunc: doPoll}
 	_, err = bp.DoPoll(ctx, h, h+deltaBlocks)
 	s.Require().NoError(err, "delegation balance not found in expected number of blocks")
+}
+
+// PollForNoDelegation polls until there is no delegation by the delegator to the validator.
+func (s *E2ETestSuite) PollForNoDelegation(
+	ctx context.Context, deltaBlocks uint64, delegatorAddress, validatorAddress string,
+) {
+	h, err := s.Chain.FuelSequencerHeight(ctx)
+	s.Require().NoError(err)
+
+	s.T().Log(fmt.Sprintf(
+		"Polling for no delegation from %s to %s",
+		delegatorAddress,
+		validatorAddress,
+	))
+
+	doPoll := func(ctx context.Context, height uint64) (any, error) {
+		res, err := s.QueryDelegationRaw(ctx, delegatorAddress, validatorAddress)
+		ok := s.Assert().ErrorContains(
+			err,
+			fmt.Sprintf("delegation with delegator %s not found for validator %s", delegatorAddress, validatorAddress),
+		)
+		if !ok {
+			if err != nil {
+				return nil, err
+			}
+			return nil, fmt.Errorf(
+				"unexpected delegation balance found (%s)", res.DelegationResponse.Balance,
+			)
+		}
+		return nil, nil
+	}
+
+	bp := BlockPoller[any]{CurrentHeight: s.Chain.FuelSequencerHeight, PollFunc: doPoll}
+	_, err = bp.DoPoll(ctx, h, h+deltaBlocks)
+	s.Require().NoError(err, "delegation balance never missing in expected number of blocks")
 }
