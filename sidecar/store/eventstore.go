7aaade3272d0148ce4f0b7519afaa5f838aeac11
migueldingli1997 <migueldingli97@gmail.com>
2024-05-06 11:35:01 +0200

    Document initial lastSyncedBlock value
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- sidecar/store/eventstore.go
+++ sidecar/store/eventstore.go
@@ -34,8 +34,10 @@ func NewEventStore(
 	maxQueryRange *big.Int,
 ) *EventStore {
 	return &EventStore{
-		blocksMap:        make(map[uint64]*sidecartypes.EthereumBlock),
-		startQueryBlock:  startQueryBlock,
+		blocksMap:       make(map[uint64]*sidecartypes.EthereumBlock),
+		startQueryBlock: startQueryBlock,
+		// The last synced block is the one right before the one we're starting at.
+		// The next query block will then evaluate to the last synced block + 1.
 		lastSyncedBlock:  new(big.Int).Sub(startQueryBlock, big.NewInt(1)),
 		maxQueryRange:    maxQueryRange,
 		blockPruneBuffer: 10,
