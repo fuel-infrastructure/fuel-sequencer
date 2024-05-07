1f7930938defb5a40ae8d5a57c207cc04bf2bdc0
dillu24 <dylangalea96@gmail.com>
2024-05-07 17:13:39 +0200

    Added all e2e tests.

--- e2e/testsuite/query_auth.go
+++ e2e/testsuite/query_auth.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"fmt"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	vestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
+	bridgetypes "github.com/fuel-infrastructure/fuel-sequencer/x/bridge/types"
 )
 
 // QueryModuleAccountAddress returns the sdk.AccAddress of a given module name.
-func (s *E2ETestSuite) QueryModuleAccountAddress(ctx context.Context, moduleName string) (sdk.AccAddress, error) {
+func (s *E2ETestSuite) QueryModuleAccountAddress(ctx context.Context, moduleName string) (sdktypes.AccAddress, error) {
 	authClient := s.getGRPCClients().AuthQueryClient
 
 	resp, err := authClient.ModuleAccountByName(ctx, &authtypes.QueryModuleAccountByNameRequest{
@@ -21,14 +23,87 @@ func (s *E2ETestSuite) QueryModuleAccountAddress(ctx context.Context, moduleName
 
 	cfg := encodingConfig
 
-	var account authtypes.AccountI
+	var account sdktypes.AccountI
 	if err := cfg.InterfaceRegistry.UnpackAny(resp.Account, &account); err != nil {
 		return nil, err
 	}
-	moduleAccount, ok := account.(authtypes.ModuleAccountI)
+	moduleAccount, ok := account.(sdktypes.ModuleAccountI)
 	if !ok {
 		return nil, fmt.Errorf("failed to cast account: %T as ModuleAccount", moduleAccount)
 	}
 
 	return moduleAccount.GetAddress(), nil
+}
+
+// QueryEthOwnedContinuousVestingAccount returns an EthOwnedContinuousVestingAccount corresponding to the given address
+// if no errors.
+func (s *E2ETestSuite) QueryEthOwnedContinuousVestingAccount(
+	ctx context.Context, address string,
+) (*bridgetypes.EthOwnedContinuousVestingAccount, error) {
+	authClient := s.getGRPCClients().AuthQueryClient
+	resp, err := authClient.Account(ctx, &authtypes.QueryAccountRequest{Address: address})
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := encodingConfig
+
+	var account sdktypes.AccountI
+	if err := cfg.InterfaceRegistry.UnpackAny(resp.Account, &account); err != nil {
+		return nil, err
+	}
+	ethOwnedContinuousVestingAccount, ok := account.(*bridgetypes.EthOwnedContinuousVestingAccount)
+	if !ok {
+		return nil, fmt.Errorf(
+			"failed to cast account: %T as EthOwnedContinuousVestingAccount", ethOwnedContinuousVestingAccount,
+		)
+	}
+
+	return ethOwnedContinuousVestingAccount, nil
+}
+
+// QueryBaseAccount returns a BaseAccount corresponding to the given address if no errors.
+func (s *E2ETestSuite) QueryBaseAccount(ctx context.Context, address string) (*authtypes.BaseAccount, error) {
+	authClient := s.getGRPCClients().AuthQueryClient
+	resp, err := authClient.Account(ctx, &authtypes.QueryAccountRequest{Address: address})
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := encodingConfig
+
+	var account sdktypes.AccountI
+	if err := cfg.InterfaceRegistry.UnpackAny(resp.Account, &account); err != nil {
+		return nil, err
+	}
+	baseAccount, ok := account.(*authtypes.BaseAccount)
+	if !ok {
+		return nil, fmt.Errorf("failed to cast account: %T as BaseAccount", baseAccount)
+	}
+
+	return baseAccount, nil
+}
+
+// QueryContinuousVestingAccount returns a ContinuousVestingAccount corresponding to the given address if no errors.
+func (s *E2ETestSuite) QueryContinuousVestingAccount(
+	ctx context.Context, address string,
+) (*vestingtypes.ContinuousVestingAccount, error) {
+	authClient := s.getGRPCClients().AuthQueryClient
+	resp, err := authClient.Account(ctx, &authtypes.QueryAccountRequest{Address: address})
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := encodingConfig
+
+	var account sdktypes.AccountI
+	if err := cfg.InterfaceRegistry.UnpackAny(resp.Account, &account); err != nil {
+		return nil, err
+	}
+	continuousVestingAccount, ok := account.(*vestingtypes.ContinuousVestingAccount)
+	if !ok {
+		return nil, fmt.Errorf("failed to cast account: %T as ContinuousVestingAccount", continuousVestingAccount)
+	}
+
+	return continuousVestingAccount, nil
 }
