787bf900f5c65e36885c89840b19c31a0546e56d
migueldingli1997 <migueldingli97@gmail.com>
2024-05-15 00:00:48 +0200

    Add TestDecodeDepositEvent
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- utils/proto_serialization_test.go
+++ utils/proto_serialization_test.go
@@ -9,6 +9,8 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/cosmos/gogoproto/proto"
+	"github.com/dvsekhvalnov/jose2go/base64url"
+	"github.com/fuel-infrastructure/fuel-sequencer/sidecar/service/types"
 	bridgetypes "github.com/fuel-infrastructure/fuel-sequencer/x/bridge/types"
 )
 
@@ -29,5 +31,28 @@ func TestProtoSerialization(t *testing.T) {
 
 	// Convert the serialized bytes to a hex string
 	hexData := fmt.Sprintf("0x%x", data)
-	fmt.Printf("Serialized Hex Data: %s", hexData)
+	fmt.Println(fmt.Printf("Serialized Hex Data: %s", hexData))
+}
+
+func TestDecodeDepositEvent(t *testing.T) {
+
+	dataBase64 := "CioweDAwNkExNzU2YWI1NzFhOWM5NjFkMjk2NTU3YmY2MGM1MGQ0OGE1MDASKjB4MDA2QTE3NTZhYjU3MWE5Yzk2MWQyOTY1NTdiZjYwYzUwZDQ4YTUwMBoTNTAwMDAwMDAwMDAwMDAwMDAwMCIDMzAw"
+	dataBz, err := base64url.Decode(dataBase64)
+	if err != nil {
+		panic(err)
+	}
+
+	var eventData types.DepositEvent
+	err = eventData.Unmarshal(dataBz)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(fmt.Sprintf(""+
+		"Depositor: %s\n"+
+		"Recipient: %s\n"+
+		"Amount: %s\n"+
+		"Lockup: %s\n",
+		eventData.Depositor, eventData.Recipient, eventData.Amount, eventData.Lockup,
+	))
 }
