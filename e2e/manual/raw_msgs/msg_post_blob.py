daeb10c78f7ec6eeada6c7965f933fbe9b858cd1
migueldingli1997 <migueldingli97@gmail.com>
2024-05-14 11:56:40 +0200

    Remove old PostBlob
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- e2e/manual/raw_msgs/msg_post_blob.py
+++ e2e/manual/raw_msgs/msg_post_blob.py
@@ -9,39 +9,6 @@ def get_msg_post_blob(
             "body": {
                 "messages": [
                     {
-                        "@type": "/fuelsequencer.sequencing.MsgPostBlob",
-                        "from": sender,
-                        "topic": topic,
-                        "order": order,
-                        "data": data
-                    }
-                ],
-                "memo": "",
-                "timeout_height": "0",
-                "extension_options": [],
-                "non_critical_extension_options": []
-            },
-            "auth_info": {
-                "signer_infos": [],
-                "fee": {
-                    "amount": fee,
-                    "gas_limit": gas,
-                    "payer": "",
-                    "granter": ""
-                }
-            },
-            "signatures": []
-        }
-
-
-def get_msg_post_blob_v1(
-        sender: str, topic: str, order: str, data: str, gas: str, fee: List,
-) -> Dict:
-    return \
-        {
-            "body": {
-                "messages": [
-                    {
                         "@type": "/fuelsequencer.sequencing.v1.MsgPostBlob",
                         "from": sender,
                         "topic": topic,
