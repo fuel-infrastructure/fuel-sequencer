eedf7ae56b03602dee95a6a5b6e7e61bd568cd86
migueldingli1997 <migueldingli97@gmail.com>
2024-05-16 18:57:24 +0100

    Refactor test
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- e2e/tests/special-messages/special_msgs_test.go
+++ e2e/tests/special-messages/special_msgs_test.go
@@ -3,7 +3,6 @@ package basic_test
 import (
 	"fmt"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/fuel-infrastructure/fuel-sequencer/e2e/testsuite"
 	bridgetypes "github.com/fuel-infrastructure/fuel-sequencer/x/bridge/types"
 )
@@ -15,61 +14,60 @@ func (s *SpecialMsgsTestSuite) TestSpecialMsgsAuthorization() {
 		// Set SupplyDeltaPeriod to a high number so that we can focus on our messages.
 		bridgeParams := s.QueryBridgeParams(s.Ctx())
 		bridgeParams.SupplyDeltaPeriod = 1000
-		msgUpdateParams := bridgetypes.MsgUpdateParams{
+		msgUpdateParams := &bridgetypes.MsgUpdateParams{
 			Authority: s.GetGovernanceAddress(),
 			Params:    *bridgeParams,
 		}
-		s.ExecuteGovProposal(&msgUpdateParams)
-
-		var msg sdk.Msg
+		s.ExecuteGovProposal(msgUpdateParams)
 
 		// Get starting block
 		fromBlock, err := s.GetFuelSequencerHeight(s.Ctx())
 		s.Require().NoError(err)
 
-		// For a control test, let's try MsgUpdateParams first
-		msg = &bridgetypes.MsgUpdateParams{
-			Authority: testsuite.ETH_ADDRESSES[0],
-			Params:    bridgetypes.DefaultParams(),
-		}
-		msgBz := s.GenerateMsgBz(msg)
-		authorizeData := testsuite.PackAuthorize(msgBz)
-		resp, err := s.SendEthTransactionToFuelStreamXContract(authorizeData)
-		s.Require().NoError(err)
+		// ------------ MsgIndex
 
-		// MsgIndex
-		msg = &bridgetypes.MsgIndex{
+		msgIndex := &bridgetypes.MsgIndex{
 			Authority:        testsuite.ADDRESSES[0],
 			NumInjectedTxs:   0,
 			NewEthereumBlock: false,
 			BlockNumber:      s.QueryLastEthereumBlockSynced(s.Ctx()) + 1,
 		}
-		msgBz = s.GenerateMsgBz(msg)
-		authorizeData = testsuite.PackAuthorize(msgBz)
-		resp, err = s.SendEthTransactionToFuelStreamXContract(authorizeData)
+		s.Require().NoError(msgIndex.ValidateBasic())
+
+		msgBz := s.GenerateMsgBz(msgIndex)
+		authorizeData := testsuite.PackAuthorize(msgBz)
+		resp, err := s.SendEthTransactionToFuelStreamXContract(authorizeData)
 		s.Require().NoError(err)
 
-		// MsgDepositFromEthereum
-		msg = &bridgetypes.MsgDepositFromEthereum{
+		// ------------ MsgDepositFromEthereum
+
+		msgDepositFromEthereum := &bridgetypes.MsgDepositFromEthereum{
 			Authority: testsuite.ADDRESSES[0],
 			Depositor: testsuite.ADDRESSES[0],
 			Recipient: testsuite.ADDRESSES[1],
 			Amount:    "1000",
 			Lockup:    "0",
 		}
-		msgBz = s.GenerateMsgBz(msg)
+		s.Require().NoError(msgDepositFromEthereum.ValidateBasic())
+
+		msgBz = s.GenerateMsgBz(msgDepositFromEthereum)
 		authorizeData = testsuite.PackAuthorize(msgBz)
 		resp, err = s.SendEthTransactionToFuelStreamXContract(authorizeData)
 		s.Require().NoError(err)
 
-		// MsgSupplyDelta
-		msg = &bridgetypes.MsgSupplyDelta{
+		// ------------ MsgSupplyDelta
+
+		msgSupplyDelta := &bridgetypes.MsgSupplyDelta{
 			Authority: testsuite.ADDRESSES[0],
 		}
-		msgBz = s.GenerateMsgBz(msg)
+		s.Require().NoError(msgSupplyDelta.ValidateBasic())
+
+		msgBz = s.GenerateMsgBz(msgSupplyDelta)
 		authorizeData = testsuite.PackAuthorize(msgBz)
 		resp, err = s.SendEthTransactionToFuelStreamXContract(authorizeData)
 		s.Require().NoError(err)
+
+		// ------------ Check results...
 
 		// Wait for all messages to get processed
 		s.PollForLastEthereumBlockSynced(s.Ctx(), 10, resp.BlockNumber.Uint64())
@@ -87,7 +85,6 @@ func (s *SpecialMsgsTestSuite) TestSpecialMsgsAuthorization() {
 	})
 
 	s.Run("Ensure special messages cannot be submitted by users", func() {
-		var msg sdk.Msg
 
 		// At CheckTx we do not apply any custom AnteHandler decorators. This means that the special messages will still
 		// need to be valid by the standards of the default AnteHandler and also pass the message handler. However, we
@@ -99,52 +96,46 @@ func (s *SpecialMsgsTestSuite) TestSpecialMsgsAuthorization() {
 		// Set SupplyDeltaPeriod to 1 so the chain expects a SupplyDelta at every block and MsgSupplyDelta passes.
 		bridgeParams := s.QueryBridgeParams(s.Ctx())
 		bridgeParams.SupplyDeltaPeriod = 1
-		msgUpdateParams := bridgetypes.MsgUpdateParams{
+		msgUpdateParams := &bridgetypes.MsgUpdateParams{
 			Authority: s.GetGovernanceAddress(),
 			Params:    *bridgeParams,
 		}
-		s.ExecuteGovProposal(&msgUpdateParams)
+		s.ExecuteGovProposal(msgUpdateParams)
 
 		// This is the error message we expect from every message.
 		invalidAuthorityErrMsg := fmt.Sprintf("invalid authority; expected %s", s.GetGovernanceAddress())
 
-		// For a control test, let's try MsgUpdateParams first
-		msg = &bridgetypes.MsgUpdateParams{
-			Authority: testsuite.ADDRESSES[0],
-			Params:    bridgetypes.DefaultParams(),
-		}
-		resp, err := s.SubmitMsgs(msg)
-		s.Require().NoError(err)
-		s.Require().Contains(resp.RawLog, invalidAuthorityErrMsg)
+		// ------------ MsgIndex
 
-		// MsgIndex
-		msg = &bridgetypes.MsgIndex{
+		msgIndex := &bridgetypes.MsgIndex{
 			Authority:        testsuite.ADDRESSES[0],
 			NumInjectedTxs:   0,
 			NewEthereumBlock: false,
 			BlockNumber:      s.QueryLastEthereumBlockSynced(s.Ctx()) + 1,
 		}
-		resp, err = s.SubmitMsgs(msg)
+		resp, err := s.SubmitMsgs(msgIndex)
 		s.Require().NoError(err)
 		s.Require().Contains(resp.RawLog, invalidAuthorityErrMsg)
 
-		// MsgDepositFromEthereum
-		msg = &bridgetypes.MsgDepositFromEthereum{
+		// ------------ MsgDepositFromEthereum
+
+		msgDepositFromEthereum := &bridgetypes.MsgDepositFromEthereum{
 			Authority: testsuite.ADDRESSES[0],
 			Depositor: testsuite.ADDRESSES[0],
 			Recipient: testsuite.ADDRESSES[1],
 			Amount:    "1000",
 			Lockup:    "0",
 		}
-		resp, err = s.SubmitMsgs(msg)
+		resp, err = s.SubmitMsgs(msgDepositFromEthereum)
 		s.Require().NoError(err)
 		s.Require().Contains(resp.RawLog, invalidAuthorityErrMsg)
 
-		// MsgSupplyDelta
-		msg = &bridgetypes.MsgSupplyDelta{
+		// ------------ MsgSupplyDelta
+
+		msgSupplyDelta := &bridgetypes.MsgSupplyDelta{
 			Authority: testsuite.ADDRESSES[0],
 		}
-		resp, err = s.SubmitMsgs(msg)
+		resp, err = s.SubmitMsgs(msgSupplyDelta)
 		s.Require().NoError(err)
 		s.Require().Contains(resp.RawLog, invalidAuthorityErrMsg)
 	})
