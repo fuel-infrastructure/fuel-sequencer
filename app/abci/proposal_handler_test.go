36dce9b1f638877225386ce26997ce607b864da0
dillu24 <dylangalea96@gmail.com>
2024-05-28 14:25:17 +0200

    Update app/abci/proposal_handler_test.go
    Co-authored-by: Miguel Dingli <migueldingli97@gmail.com>

--- app/abci/proposal_handler_test.go
+++ app/abci/proposal_handler_test.go
@@ -1048,7 +1048,7 @@ func (s *AppTestSuite) TestProcessProposalHandler() {
 			},
 			supplyDeltaPeriod:            testtypes.TestSupplyDeltaPeriod,
 			ethereumProxyContractAddress: testtypes.TestEthereumProxyContractAddress,
-			injectedEventTxMaxBytes:      1, // To ensure authorize event is big enough
+			injectedEventTxMaxBytes:      1, // To ensure deposit event is too big
 			maxBlockGas:                  totalTxsGas,
 		},
 		{
