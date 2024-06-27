fce88945704b46404281e6cb91cbb370b325d8b3
Dylan Frendo <18212336+dferendo@users.noreply.github.com>
2024-06-27 11:14:58 +0200

    Update app/abci/proposal_handler.go
    Co-authored-by: dillu24 <dylangalea96@gmail.com>

--- app/abci/proposal_handler.go
+++ app/abci/proposal_handler.go
@@ -249,7 +249,7 @@ func (h *FuelSequencerProposalHandler) ProcessProposalHandler() sdk.ProcessPropo
 		var partialEthereumSyncing = !injectedMsgIndex.NewEthereumBlock && injectedMsgIndex.NumInjectedEventTxs > 0
 
 		if !fullEthereumSyncing && !partialEthereumSyncing {
-			// No new block syncing, we only accept these blocks if MaxEthBlockUpdateDelay is not exceeded
+			// No Ethereum syncing, therefore, we only accept this block if MaxEthBlockUpdateDelay is not exceeded
 			lastEthBlockUpdateTime, found := h.bridgeKeeper.GetLastEthBlockUpdateTime(ctx)
 			ethSyncDelayExceeded := found && req.Time.After(lastEthBlockUpdateTime.Add(bridgeParams.MaxEthBlockUpdateDelay))
 
