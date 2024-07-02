96a3dc643dcb32356dbf8df088deb41df058b151
migueldingli1997 <migueldingli97@gmail.com>
2024-07-02 09:39:52 +0200

    Improve GetAllBlockedAddresses test
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- x/bridge/keeper/bridge_utils_test.go
+++ x/bridge/keeper/bridge_utils_test.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 
 	sdkmath "cosmossdk.io/math"
+	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
+	"github.com/cosmos/cosmos-sdk/x/staking/testutil"
 	testtypes "github.com/fuel-infrastructure/fuel-sequencer/testutil/types"
 )
 
@@ -100,8 +101,21 @@ func (s *KeeperTestSuite) TestGetAllBlockedAddresses() {
 
 			ctx := s.Ctx()
 
+			// Create validator which will not get included in the GetLastValidators call
+			valPubKey := simtestutil.CreateTestPubKeys(1)[0]
+			valAddr := sdk.ValAddress(valPubKey.Address().Bytes())
+			addTokens := s.App.StakingKeeper.TokensFromConsensusPower(ctx, 10)
+			extraValidator := testutil.NewValidator(s.T(), valAddr, valPubKey)
+			extraValidator, _, err := s.App.StakingKeeper.AddValidatorTokensAndShares(ctx, extraValidator, addTokens)
+			s.Require().NoError(err)
+			s.Require().Equal(addTokens, extraValidator.Tokens)
+
 			vals, err := s.App.StakingKeeper.GetLastValidators(ctx)
 			s.Require().NoError(err)
+
+			// Confirm the difference between the 'LastValidators' and the full list of validators.
+			allVals, err := s.App.StakingKeeper.GetAllValidators(ctx)
+			s.Require().Greater(len(allVals), len(vals))
 
 			// Verify that there is at least one validator
 			s.Require().GreaterOrEqual(len(vals), 1)
@@ -113,6 +127,12 @@ func (s *KeeperTestSuite) TestGetAllBlockedAddresses() {
 				accAddrFromValAddr := sdk.AccAddress(valAddr.Bytes()).String()
 				tc.expectedBlockedAddresses = append(tc.expectedBlockedAddresses, accAddrFromValAddr)
 			}
+
+			// 'Extra' validator is not blocked
+			extraValAddr, err := sdk.ValAddressFromBech32(extraValidator.OperatorAddress)
+			s.Require().NoError(err)
+			accAddrFromExtraValAddr := sdk.AccAddress(extraValAddr.Bytes()).String()
+			tc.notBlockedAddresses = append(tc.notBlockedAddresses, accAddrFromExtraValAddr)
 
 			// The authority address is blocked
 			tc.expectedBlockedAddresses = append(tc.expectedBlockedAddresses, s.App.BridgeKeeper.GetAuthority())
