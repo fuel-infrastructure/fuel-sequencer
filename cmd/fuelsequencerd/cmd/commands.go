10a5f7a78f85a2b45f233d51aa748373f50b898a
Dylan Frendo <18212336+dferendo@users.noreply.github.com>
2024-07-02 12:57:06 +0200

    Update cmd/fuelsequencerd/cmd/commands.go
    Co-authored-by: Miguel Dingli <migueldingli97@gmail.com>

--- cmd/fuelsequencerd/cmd/commands.go
+++ cmd/fuelsequencerd/cmd/commands.go
@@ -170,7 +170,7 @@ func startSidecarServerCmd() *cobra.Command {
 	cmd.Flags().Int64Var(&ethCfg.maxBlockRange, FlagEthereumMaxBlockRange, 100, "max number of Ethereum blocks queried at one go")
 	cmd.Flags().DurationVar(&ethCfg.minLogsQueryInterval, FlagEthereumMinLogsQueryInterval, time.Second*5, "minimum wait between successive queries for logs")
 	cmd.Flags().Int64Var(&ethCfg.unsafeStartBlock, FlagEthereumUnsafeStartBlock, 0, "the Ethereum block to start querying from")
-	cmd.Flags().Int64Var(&ethCfg.unsafeEndBlock, FlagEthereumUnsafeEndBlock, 0, "the last Ethereum block to query. Syncing stops after this block, which can cause the validator to propose empty blocks, leading to slashing")
+	cmd.Flags().Int64Var(&ethCfg.unsafeEndBlock, FlagEthereumUnsafeEndBlock, 0, "the last Ethereum block to sync - incorrect use can cause the validator to propose empty blocks, leading to slashing!")
 
 	// Sequencer
 	cmd.Flags().StringVar(&seqCfg.grpcUrl, FlagSequencerGrpcUrl, "127.0.0.1:9090", "the sequencer's gRPC endpoint")
