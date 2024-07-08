0db23481f097c208c5f07e2309c75dcadba103f5
dillu24 <dylangalea96@gmail.com>
2024-07-08 16:28:09 +0200

    Added genesis fix.

--- app/ante.go
+++ app/ante.go
@@ -124,6 +124,12 @@ func NewSequencerNativeTxsDecorator(sequencingKeeper sequencingkeeper.Keeper) Se
 func (d SequencerNativeTxsDecorator) AnteHandle(
 	ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler,
 ) (sdk.Context, error) {
+	// Limits should not be applied during genesis as params.SequencerTxMaxBytes will be zero until the genesis values
+	// are set.
+	if ctx.BlockHeight() == 0 {
+		return next(ctx, tx, simulate)
+	}
+
 	txSize := uint64(len(ctx.TxBytes()))
 	params := d.sequencingKeeper.GetParams(ctx)
 
