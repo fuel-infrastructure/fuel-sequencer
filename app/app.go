fb6868581d66c38ac807a8078d14cfd40570109d
migueldingli1997 <migueldingli97@gmail.com>
2024-07-10 17:59:42 +0200

    Refactor
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- app/app.go
+++ app/app.go
@@ -62,6 +62,7 @@ import (
 
 	bridgemodulekeeper "github.com/fuel-infrastructure/fuel-sequencer/x/bridge/keeper"
 	sequencingmodulekeeper "github.com/fuel-infrastructure/fuel-sequencer/x/sequencing/keeper"
+
 	// this line is used by starport scaffolding # stargate/app/moduleImport
 
 	"github.com/fuel-infrastructure/fuel-sequencer/client/docs"
