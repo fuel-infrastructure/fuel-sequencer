848fb6fb11d25f1cbac950aed44175d027a05201
migueldingli1997 <migueldingli97@gmail.com>
2024-07-25 14:34:59 +0200

    Improve average block time calculation and add another report
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- e2e/manual/run_average_block_time.py
+++ e2e/manual/run_average_block_time.py
@@ -1,14 +1,36 @@
 import time
+from typing import Optional, Union
 
 import requests
 from dateutil import parser
 
-sequencer_rpc = "https://rpc-seq.simplystaking.xyz/"
-block_query = "block?height="
+sequencer_rpc = "https://rpc-seq.simplystaking.xyz"
+sequencer_rest = "https://rest-seq.simplystaking.xyz"
+commit_query = "/commit?height="
+block_query = "/block?height="
+account_query = "/cosmos/auth/v1beta1/account_info/"
+
+
+def get_account(address: str, height: Optional[Union[str, int]] = None):
+    headers = {
+        "x-cosmos-block-height": str(height)
+    } if height is not None else {}
+    return requests.get(
+        f"{sequencer_rest}{account_query}{address}",
+        headers=headers,
+    ).json()
 
 
 def get_block(n):
     return requests.get(f"{sequencer_rpc}{block_query}{n}").json()['result']
+
+
+def get_commit(n):
+    return requests.get(f"{sequencer_rpc}{commit_query}{n}").json()['result']
+
+
+def get_latest_commit():
+    return get_commit("")
 
 
 def get_latest_block():
@@ -16,15 +38,29 @@ def get_latest_block():
 
 
 # Calculate block range based on latest height
-latest_block = get_latest_block()
-latest_height = int(latest_block['block']['header']['height'])
+latest_block = get_latest_commit()
+latest_height = int(latest_block['signed_header']['header']['height'])
 end_heights = [latest_height]
 start_heights = [latest_height - 200]
-
 
 # Optional: manual override of heights
 # start_heights = []
 # end_heights = []
+
+# Get number of transactions in each block range for these signers
+signers = [
+    # "fuelsequencer1vtfzrk6f4m6kxt6ehyqt9j5su5hvcz5q3dmlsm",  # alice
+    # "fuelsequencer1ptrdx8rzykw57suy540tlhfhxfclmykywhtycf",  # temp1
+    # "fuelsequencer1z6mj9pgyd0gdt7zpnn5k5xgw3u99ammylkh0tn",  # temp2
+    # "fuelsequencer18q22y7cyxsev5ttqkvqt6dse0zd6zejyqja2h0",  # temp3
+    # "fuelsequencer1wnfm34d2wees97cl34qcev8de3ja2n8whh732u",  # temp4
+    # "fuelsequencer1h4dr6td79wagkgt5z8qhzaazh30nnavgq8g7wf",  # temp5
+    # "fuelsequencer1w683zjxx9pvnceakafaf3penc060uhggdqjsw3",  # temp6
+    # "fuelsequencer16eqy4fjtrr5rf8e0gfsc48qlzdpfl5enng849y",  # temp7
+    # "fuelsequencer1ngsagnggmmumc62220n6waav5wzjgm8hf8kzh0",  # temp8
+    # "fuelsequencer1cyvqtp695llvpcu08xg5ndjsqre0nsash972vz",  # temp9
+    # "fuelsequencer1lut8dr0473pxm9ayynay7hhya9n8dnsa6egh5p",  # temp10
+]
 
 
 def base64_decoded_size(encoded_str):
@@ -34,10 +70,14 @@ def base64_decoded_size(encoded_str):
 
 
 def run_report_1(start_height: int, end_height: int):
-    start_block = get_block(start_height)
-    start_block_timestamp = parser.parse(start_block['block']['header']['time'])
-    end_block = get_block(end_height)
-    end_block_timestamp = parser.parse(end_block['block']['header']['time'])
+    start_block = get_commit(start_height)
+    start_block_timestamp = parser.parse(
+        start_block['signed_header']['header']['time']
+    )
+    end_block = get_commit(end_height)
+    end_block_timestamp = parser.parse(
+        end_block['signed_header']['header']['time']
+    )
     block_range_time = (end_block_timestamp - start_block_timestamp)
     seconds_per_block = block_range_time / (end_height - start_height)
     print(
@@ -48,7 +88,16 @@ def run_report_1(start_height: int, end_height: int):
     )
 
 
-def run_report_2(start_height: int, end_height: int):
+def run_report_2(start_height: int, end_height: int, address: str):
+    sequence1 = int(get_account(address, start_height)['info']['sequence'])
+    sequence2 = int(get_account(address, end_height)['info']['sequence'])
+    print(
+        f"Transactions from {start_height} to {end_height} ({address}): "
+        f"{sequence2 - sequence1}"
+    )
+
+
+def run_report_3(start_height: int, end_height: int):
     previous_block_timestamp = None
     for n in range(start_height, end_height):
 
@@ -75,10 +124,16 @@ def run_report_2(start_height: int, end_height: int):
 
 # Report 1
 for i in range(len(start_heights)):
-    print("---")
+    print(f"--- (Report 1.{i})")
     run_report_1(start_heights[i], end_heights[i])
 
 # Report 2
+for i, signer in enumerate(signers):
+    print(f"--- (Report 2.{i})")
+    for i in range(len(start_heights)):
+        run_report_2(start_heights[i], end_heights[i], signer)
+
+# Report 3
 for i in range(len(start_heights)):
-    print("---")
-    run_report_2(start_heights[i], end_heights[i])
+    print(f"--- (Report 3.{i})")
+    run_report_3(start_heights[i], end_heights[i])
