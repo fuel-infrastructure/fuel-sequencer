c4fdf6bd7dc53c24187fb575910783ab0045ac9b
migueldingli1997 <migueldingli97@gmail.com>
2024-07-26 17:33:08 +0200

    Revert "Add more logging to proposer at PrepareProposal"
    This reverts commit a436d9d98c20d5e8bc4677f501e797e92d6ab671.

--- app/abci/proposal_handler.go
+++ app/abci/proposal_handler.go
@@ -72,9 +72,6 @@ func NewFuelSequencerProposalHandler(
 // Reference: https://github.com/cosmos/cosmos-sdk/blob/a248d05f70f4ad7b8ff7b521e3d23086867d07dc/baseapp/abci.go#L447-L451
 func (h *FuelSequencerProposalHandler) PrepareProposalHandler() sdk.PrepareProposalHandler {
 	return func(ctx sdk.Context, req *abci.RequestPrepareProposal) (*abci.ResponsePrepareProposal, error) {
-		proposerConsAddress := sdk.ConsAddress(req.ProposerAddress)
-		ctx.Logger().Info("preparing proposal", "proposer", proposerConsAddress, "num_txs", len(req.Txs))
-
 		bridgeParams := h.bridgeKeeper.GetParams(ctx)
 
 		blockedBech32Addresses, err := h.bridgeKeeper.GetAllBlockedBech32Addresses(ctx)
@@ -123,8 +120,6 @@ func (h *FuelSequencerProposalHandler) PrepareProposalHandler() sdk.PreparePropo
 		if sidecarErr != nil {
 			ctx.Logger().Warn("observed sidecar error at PrepareProposal", "err", sidecarErr)
 			// This error is also passed to generateMsgIndexAndEventTxs to perform dedicated error handling.
-		} else {
-			ctx.Logger().Info("received sidecar response at PrepareProposal", "num_events", len(response.Events))
 		}
 
 		msgIndex, eventTxs, err := h.generateMsgIndexAndEventTxs(
