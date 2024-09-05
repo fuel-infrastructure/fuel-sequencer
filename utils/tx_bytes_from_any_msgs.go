00fc28a0f78acffb34926907f6e54c73b3d4ad88
migueldingli1997 <migueldingli97@gmail.com>
2024-09-05 17:51:53 +0200

    docs: document sequence into ValidRawTxBytesFromAnyMsgs
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- utils/tx_bytes_from_any_msgs.go
+++ utils/tx_bytes_from_any_msgs.go
@@ -18,7 +18,8 @@ const (
 // it will contribute to the block's last results hash and thus be provable on Ethereum via Bridge Commitments.
 //
 // The transaction's gas limit is set to zero with the assumption that an infinite gas meter will be used.
-// TODO: document the sequence
+//
+// A sequence, presumed to be unique, is also specified to ensure that the generated transaction has a unique tx hash.
 func ValidRawTxBytesFromAnyMsgs(msgs []*codectypes.Any, sequence uint64) ([]byte, error) {
 
 	// Construct Tx Body with the message.
