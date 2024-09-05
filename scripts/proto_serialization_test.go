eeef0a306f16cfd965541bbb627692e52c620bd0
migueldingli1997 <migueldingli97@gmail.com>
2024-09-05 23:26:19 +0200

    scripts: add tx hash to proto_serialization_test.go
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- scripts/proto_serialization_test.go
+++ scripts/proto_serialization_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	sdkmath "cosmossdk.io/math"
+	"github.com/cometbft/cometbft/types"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtx "github.com/cosmos/cosmos-sdk/x/auth/tx"
@@ -149,12 +150,13 @@ func TestEncodeDepositEvent(t *testing.T) {
 
 func TestDecodeTx_Base64(t *testing.T) {
 
-	dataBase64 := "Cl8KXQohL2Z1ZWxzZXF1ZW5jZXIuYnJpZGdlLnYxLk1zZ0luZGV4EjgKNGZ1ZWxzZXF1ZW5jZXIxMGQwN3kyNjVnbW11dnQ0ejB3OWF3ODgwam5zcjcwMGpkamZ2azMgARICEgA="
+	dataBase64 := "CogBCoUBChwvY29zbW9zLmJhbmsudjFiZXRhMS5Nc2dTZW5kEmUKKjB4ZjM5RmQ2ZTUxYWFkODhGNkY0Y2U2YUI4ODI3Mjc5Y2ZmRmI5MjI2NhIqMHhkNDQ3MDY2YThiYTljYjE1YTg2MmEwZjZkZTk2MWYyN2JlODZmYzBhGgsKBXV0ZXN0EgIxMBIGCgIYVxIA"
 	dataBz, err := base64.StdEncoding.DecodeString(dataBase64)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("SIZE: %d\n", utils.TxSize(dataBz))
+	fmt.Printf("HASH: %X\n", types.Tx(dataBz).Hash())
 
 	tx, err := authtx.DefaultTxDecoder(testutiltypes.TestCdc)(dataBz)
 	if err != nil {
@@ -174,6 +176,7 @@ func TestDecodeTx_Hex(t *testing.T) {
 		panic(err)
 	}
 	fmt.Printf("SIZE: %d\n", utils.TxSize(dataBz))
+	fmt.Printf("HASH: %X\n", types.Tx(dataBz).Hash())
 
 	tx, err := authtx.DefaultTxDecoder(testutiltypes.TestCdc)(dataBz)
 	if err != nil {
