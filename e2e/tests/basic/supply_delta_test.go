ec549a207806fe7a797a74cd0cd1e7b54dd6e31c
migueldingli1997 <migueldingli97@gmail.com>
2024-09-05 16:52:28 +0200

    e2e: add unique tx hash checks for supply delta
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- e2e/tests/basic/supply_delta_test.go
+++ e2e/tests/basic/supply_delta_test.go
@@ -3,6 +3,8 @@ package basic_test
 import (
 	"time"
 
+	cmtbytes "github.com/cometbft/cometbft/libs/bytes"
+	"github.com/cometbft/cometbft/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	bridgetypes "github.com/fuel-infrastructure/fuel-sequencer/x/bridge/types"
 )
@@ -23,18 +25,25 @@ func (s *BasicTestSuite) TestMsgSupplyDeltaIsInjected() {
 		typicalMsgSupplyDelta := bridgetypes.MsgSupplyDelta{Authority: s.GetGovernanceAddress()}
 		msgSupplyDeltaTypeUrl := sdk.MsgTypeURL(&bridgetypes.MsgSupplyDelta{})
 
-		// Ensure that MsgSupplyDelta injected, and only at the right heights
+		// Ensure that MsgSupplyDelta injected, and only at the right heights.
+		// Also ensure that all MsgSupplyDelta transaction hashes are unique.
+		uniqueHashes := make(map[string]bool)
 		for block := int64(1); block < searchTillBlock; block++ {
 			expectMsgSupplyDelta := block%supplyDeltaPeriod == 0
 
 			// Search for message showing successful injection
-			msg, msgFound := s.SearchForMsgInBlock(s.Ctx(), msgSupplyDeltaTypeUrl, block)
+			msg, txBz, msgFound := s.SearchForMsgInBlock(s.Ctx(), msgSupplyDeltaTypeUrl, block)
 			s.Require().Equal(expectMsgSupplyDelta, msgFound)
 			if expectMsgSupplyDelta {
 				msgSupplyDelta, ok := msg.(*bridgetypes.MsgSupplyDelta)
 				s.Require().True(ok)
 				s.Require().EqualValues(typicalMsgSupplyDelta, *msgSupplyDelta)
 			}
+			txHash := cmtbytes.HexBytes(types.Tx(txBz).Hash()).String()
+			if _, alreadySeen := uniqueHashes[txHash]; alreadySeen {
+				s.Require().False(alreadySeen)
+			}
+			uniqueHashes[txHash] = true
 
 			// Search for event showing successful execution
 			event, eventFound := s.SearchForEventInBlockResults(s.Ctx(), supplyDeltaEvent.Type, block)
