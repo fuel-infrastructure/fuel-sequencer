afcc2f24df0086c6c565a8c911c306970fe7f890
migueldingli1997 <migueldingli97@gmail.com>
2024-09-19 14:38:48 +0200

    Add timeout config for Prometheus server
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- cmd/fuelsequencerd/cmd/commands.go
+++ cmd/fuelsequencerd/cmd/commands.go
@@ -241,6 +241,9 @@ func startSidecarServerCmd() *cobra.Command {
 	cmd.Flags().DurationVar(
 		&prmCfg.ReadHeaderTimeout, FlagPrometheusReadHeaderTimeout, time.Second*10, "amount of time allowed to read request headers",
 	)
+	cmd.Flags().DurationVar(
+		&prmCfg.WriteTimeout, FlagPrometheusWriteTimeout, time.Second*10, "maximum duration before timing out writes of the response",
+	)
 	cmd.Flags().StringVar(
 		&prmCfg.Namespace, FlagPrometheusNamespace, "sidecar", "instrumentation namespace",
 	)
