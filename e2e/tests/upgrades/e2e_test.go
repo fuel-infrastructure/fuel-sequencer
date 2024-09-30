e5ec583447c26288d8a072544d2980d31d80f973
migueldingli1997 <migueldingli97@gmail.com>
2024-09-30 17:03:21 +0200

    Ensure we can delegate after adjusting the power reduction
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- e2e/tests/upgrades/e2e_test.go
+++ e2e/tests/upgrades/e2e_test.go
@@ -1,10 +1,14 @@
 package upgrades_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
+	sdkmath "cosmossdk.io/math"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/fuel-infrastructure/fuel-sequencer/app/upgrades/power_reduction"
 	e2etestsuite "github.com/fuel-infrastructure/fuel-sequencer/e2e/testsuite"
 	"github.com/stretchr/testify/suite"
@@ -36,41 +40,84 @@ func (s *UpgradesTestSuite) SetupTest() {
 
 func (s *UpgradesTestSuite) TestUpgradePowerReduction() {
 
-	height, err := s.GetFuelSequencerHeight(s.Ctx())
-	s.Require().NoError(err, "error fetching height before submit upgrade proposal")
+	s.Run("Perform the upgrade", func() {
+		height, err := s.GetFuelSequencerHeight(s.Ctx())
+		s.Require().NoError(err, "error fetching height before submit upgrade proposal")
 
-	haltHeight := height + haltHeightDelta
-	s.Logger().Info("Submitting software upgrade proposal", zap.Uint64("halt_height", haltHeight))
-	msgUpgrade := &upgradetypes.MsgSoftwareUpgrade{
-		Authority: s.GetGovernanceAddress(),
-		Plan: upgradetypes.Plan{
-			Name:   upgradeName,
-			Height: int64(haltHeight),
-			Info:   "<dummy-info>",
-		},
-	}
-	s.ExecuteGovProposal(msgUpgrade)
+		haltHeight := height + haltHeightDelta
+		s.Logger().Info("Submitting software upgrade proposal", zap.Uint64("halt_height", haltHeight))
+		msgUpgrade := &upgradetypes.MsgSoftwareUpgrade{
+			Authority: s.GetGovernanceAddress(),
+			Plan: upgradetypes.Plan{
+				Name:   upgradeName,
+				Height: int64(haltHeight),
+				Info:   "<dummy-info>",
+			},
+		}
+		s.ExecuteGovProposal(msgUpgrade)
 
-	height, err = s.GetFuelSequencerHeight(s.Ctx())
-	s.Require().NoError(err, "error fetching height before upgrade")
+		height, err = s.GetFuelSequencerHeight(s.Ctx())
+		s.Require().NoError(err, "error fetching height before upgrade")
 
-	// Wait until just before the upgrade
-	err = s.WaitUntilSequencerBlock(s.Ctx(), int(haltHeight-1), time.Second*20)
-	s.Require().NoError(err)
+		// Wait until just before the upgrade
+		err = s.WaitUntilSequencerBlock(s.Ctx(), int(haltHeight-1), time.Second*20)
+		s.Require().NoError(err)
 
-	// Ensure the nodes have reached the halt height
-	time.Sleep(time.Second * 5)
+		// Ensure the nodes have reached the halt height
+		time.Sleep(time.Second * 5)
 
-	// Bring down nodes to prepare for upgrade.
-	s.Logger().Info("Stopping all sequencer nodes...")
-	s.StopAllSequencerNodes()
-	s.Logger().Info("Removing all sequencer nodes...")
-	s.RemoveAllSequencerNodes()
+		// Bring down nodes to prepare for upgrade.
+		s.Logger().Info("Stopping all sequencer nodes...")
+		s.StopAllSequencerNodes()
+		s.Logger().Info("Removing all sequencer nodes...")
+		s.RemoveAllSequencerNodes()
 
-	// Upgrade version on all nodes and start them back up.
-	s.FuelSequencerDockerImageTag = toImageVersion
-	s.RunFuelSequencerValidators()
+		// Upgrade version on all nodes and start them back up.
+		s.Logger().Info("Starting nodes back up...")
+		s.FuelSequencerDockerImageTag = toImageVersion
+		s.RunFuelSequencerValidators()
 
-	err = s.WaitForSequencerBlocks(s.Ctx(), int(blocksAfterUpgrade), time.Second*20)
-	s.Require().NoError(err, "chain did not produce blocks after upgrade")
+		err = s.WaitForSequencerBlocks(s.Ctx(), int(blocksAfterUpgrade), time.Second*20)
+		s.Require().NoError(err, "chain did not produce blocks after upgrade")
+	})
+
+	s.Run("Ensure we can perform a large delegation", func() {
+
+		senderAddress := s.SeqKeys[0].AddressSeq
+		delegatorKey := s.EthUser.PrivateKey
+		delegatorAddress := s.EthUser.AddressHex
+		validator1Address := s.SeqKeys[0].ValAddressSeq
+
+		// --------------------------------------- Fund the delegator
+
+		amount, ok := sdkmath.NewIntFromString("10000000000000000000000000000") // 10 bil x 1e18
+		s.Require().True(ok)
+		delegation := sdk.NewCoin(e2etestsuite.BridgeDenom, amount)
+		msgSend := &banktypes.MsgSend{
+			FromAddress: senderAddress,
+			ToAddress:   delegatorAddress,
+			Amount:      sdk.NewCoins(delegation),
+		}
+		_, err := s.SubmitMsgs(msgSend)
+		s.Require().NoError(err)
+
+		// --------------------------------------- Delegate
+
+		// Make sure that there is no pre-existing delegation between the delegator and validator1.
+		delegationRaw, err := s.QueryDelegationRaw(s.Ctx(), delegatorAddress, validator1Address)
+		s.Require().Nil(delegationRaw)
+		s.Require().ErrorContains(
+			err,
+			fmt.Sprintf("delegation with delegator %s not found for validator %s", delegatorAddress, validator1Address),
+		)
+
+		// Generate Authorize event wrapping a MsgDelegate to validator1.
+		msgDelegateBz := s.E2ETestSuite.GenerateMsgDelegateBz(delegatorAddress, validator1Address, delegation)
+		authorizeData := e2etestsuite.PackAuthorize(msgDelegateBz)
+		_, err = s.SendEthTransactionFrom(delegatorKey, e2etestsuite.SequencerInterfaceContractAddress, authorizeData)
+		s.Require().NoError(err)
+
+		// Confirm that the delegation went through and is as expected.
+		s.PollForDelegationBalance(s.Ctx(), 30, delegatorAddress, validator1Address, delegation)
+	})
 }
