f52bf97ca11c91f31f4534ee9655c1571de4f949
Miguel Dingli <migueldingli97@gmail.com>
2024-10-01 14:36:52 +0200

    Update app/app.go
    Co-authored-by: dillu24 <dylangalea96@gmail.com>

--- app/app.go
+++ app/app.go
@@ -133,6 +133,7 @@ func init() {
 	// DefaultPowerReduction is the amount of staking tokens required for 1 unit of consensus-engine power.
 	// We change this to 1e18 to match the number of decimal places used in the bridged token denomination.
 	// There are plans to change this to an on-chain param: https://github.com/cosmos/cosmos-sdk/issues/8365
+	// Note: This should be revised if the number of decimals for the bridged token changes.
 	sdk.DefaultPowerReduction = sdkmath.NewIntFromUint64(1000000000000000000)
 }
 
