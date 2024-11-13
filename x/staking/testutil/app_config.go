40a5012f4b24a3a62ae0e21edc205c9eb31e3999
dillu24 <dylangalea96@gmail.com>
2024-11-13 10:34:01 +0100

    Fixed staking unit tests.

--- x/staking/testutil/app_config.go
+++ x/staking/testutil/app_config.go
@@ -1,6 +1,9 @@
 package testutil
 
 import (
+	appv1alpha1 "cosmossdk.io/api/cosmos/app/v1alpha1"
+	mintmodulev1 "cosmossdk.io/api/cosmos/mint/module/v1"
+	"cosmossdk.io/core/appconfig"
 	"github.com/cosmos/cosmos-sdk/testutil/configurator"
 	_ "github.com/cosmos/cosmos-sdk/x/auth"                     // import as blank for app wiring
 	_ "github.com/cosmos/cosmos-sdk/x/auth/tx/config"           // import as blank for app wiring
@@ -14,6 +17,23 @@ import (
 	_ "github.com/fuel-infrastructure/fuel-sequencer/x/staking" // import as blank for app wiring
 )
 
+// MintModule defines a custom configuration for the mint module. We cannot use the Cosmos SDK configurator because it
+// points to the Cosmos SDKs staking module.
+func MintModule() configurator.ModuleOption {
+	return func(config *configurator.Config) {
+		config.ModuleConfigs["mint"] = &appv1alpha1.ModuleConfig{
+			Name:   "mint",
+			Config: appconfig.WrapAny(&mintmodulev1.Module{}),
+			GolangBindings: []*appv1alpha1.GolangBinding{
+				{
+					InterfaceType:  "github.com/cosmos/cosmos-sdk/x/mint/types/types.StakingKeeper",
+					Implementation: "github.com/fuel-infrastructure/fuel-sequencer/x/staking/keeper/*keeper.Keeper",
+				},
+			},
+		}
+	}
+}
+
 var AppConfig = configurator.NewAppConfig(
 	configurator.AuthModule(),
 	configurator.BankModule(),
@@ -22,6 +42,6 @@ var AppConfig = configurator.NewAppConfig(
 	configurator.ConsensusModule(),
 	configurator.ParamsModule(),
 	configurator.GenutilModule(),
-	configurator.MintModule(),
+	MintModule(),
 	configurator.DistributionModule(),
 )
