724c634dc1af1c64fc325a4f7b4616534c76738f
dillu24 <dylangalea96@gmail.com>
2024-12-09 16:33:31 +0100

    Added AfterUnbondingDelegationSlashed hook logic and unit tests.

--- x/reports/keeper/hooks.go
+++ x/reports/keeper/hooks.go
@@ -2,10 +2,12 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
+	"cosmossdk.io/errors"
 	sdkmath "cosmossdk.io/math"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/fuel-infrastructure/fuel-sequencer/x/reports/types"
 )
 
@@ -66,12 +68,57 @@ func (h Hooks) AfterUnbondingInitiated(_ context.Context, _ uint64) error {
 }
 
 func (h Hooks) AfterUnbondingDelegationSlashed(
-	_ context.Context, _ sdk.ValAddress, _ sdk.AccAddress, _ sdkmath.Int,
+	ctx context.Context, valAddr sdk.ValAddress, delAddr sdk.AccAddress, slashAmount sdkmath.Int,
 ) error {
-	// TODO: Impl
-	// TODO: PANIC if error occurs
-	// TODO: Don't forget to call validateBasic of slash entry
-	// TODO: Height is ctx.blockHeight
-	// TODO: First see if a slash entry exists for height
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+
+	// Make sure that the height is positive
+	// Given that slashing is not expected at heights less or equal to zero we will panic if this occurs.
+	if sdkCtx.BlockHeight() <= 0 {
+		panic(sdkerrors.ErrInvalidHeight.Wrapf("slashing height must be positive, received: %v", sdkCtx.BlockHeight()))
+	}
+
+	// Compute height, validator address and delegator address.
+	// Note: uint64 is bigger than int64, therefore, the height can be converted safely to uint64.
+	height := uint64(sdkCtx.BlockHeight())
+	validatorAddress := valAddr.String()
+	delegatorAddress := delAddr.String()
+
+	// Make sure that slashed amount is positive. We are panicking here because this is highly unexpected, as the forked
+	// Cosmos SDK triggers the AfterUnbondingDelegationSlashed hook only if the burned amount is positive.
+	if !slashAmount.IsPositive() {
+		panic(fmt.Errorf("slash amount must be positive, received %s", slashAmount.String()))
+	}
+
+	// Check if a slash entry already exists in state for the current height.
+	slashEntry, found := h.k.GetSlashEntry(ctx, height, delegatorAddress, validatorAddress)
+	if found {
+		// If a slash entry already exists, increment the slashed amount as it must be that the delegator has already
+		// been slashed at this height. Set the unbonding and bonded balances to zero as that should be computed by the
+		// reports module BeginBlocker.
+		slashEntry.DelegatorSlashAmount = slashEntry.DelegatorSlashAmount.Add(slashAmount)
+		slashEntry.DelegatorBondedBalance = sdkmath.ZeroInt()
+		slashEntry.DelegatorUnbondingBalance = sdkmath.ZeroInt()
+	} else {
+		// Otherwise create a new slash entry. For the same reason as the found=true case, delegator bonded and
+		// unbonding balances should be set to zero.
+		slashEntry = types.SlashEntry{
+			ValidatorAddress:          validatorAddress,
+			DelegatorAddress:          delegatorAddress,
+			DelegatorSlashAmount:      slashAmount,
+			DelegatorBondedBalance:    sdkmath.ZeroInt(),
+			DelegatorUnbondingBalance: sdkmath.ZeroInt(),
+		}
+	}
+
+	// Make sure that the slash entry satisfies the basic validation checks and panic if not as this would mean that
+	// there is a bug in the code. We need to panic because the staking module delegates panicking responsibilities to
+	// the individual modules.
+	if err := slashEntry.ValidateBasic(); err != nil {
+		panic(errors.Wrap(err, "AfterUnbondingDelegationSlashed: constructed invalid slash entry"))
+	}
+
+	h.k.SetSlashEntry(ctx, height, slashEntry)
+
 	return nil
 }
--- x/reports/keeper/hooks_test.go
+++ x/reports/keeper/hooks_test.go
@@ -1,3 +1,174 @@
-package keeper
+package keeper_test
 
-// TODO
+import (
+	"fmt"
+
+	sdkmath "cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	testtypes "github.com/fuel-infrastructure/fuel-sequencer/testutil/types"
+	"github.com/fuel-infrastructure/fuel-sequencer/x/reports/types"
+)
+
+func (s *KeeperTestSuite) TestAfterUnbondingDelegationSlashed() {
+	height := int64(10)
+	delegatorAccAddress := testtypes.TestSeqAddr1
+	validatorAddress, err := sdk.ValAddressFromBech32(testtypes.TestValAddr1Str)
+	s.Require().NoError(err)
+
+	testCases := []struct {
+		name          string
+		setSlashEntry *types.SlashEntry
+		height        int64
+		delAddr       sdk.AccAddress
+		valAddr       sdk.ValAddress
+		slashAmount   sdkmath.Int
+		expSlashEntry types.SlashEntry
+		expErrMsg     string
+		expPanic      bool
+	}{
+		{
+			name:        "slash entry set correctly first time",
+			height:      height,
+			delAddr:     delegatorAccAddress,
+			valAddr:     validatorAddress,
+			slashAmount: sdkmath.OneInt(),
+			expSlashEntry: types.SlashEntry{
+				ValidatorAddress:          testtypes.TestValAddr1Str,
+				DelegatorAddress:          testtypes.TestSeqAddr1Str,
+				DelegatorSlashAmount:      sdkmath.OneInt(),
+				DelegatorBondedBalance:    sdkmath.ZeroInt(),
+				DelegatorUnbondingBalance: sdkmath.ZeroInt(),
+			},
+		},
+		{
+			name: "slash entry updated correctly if already exists",
+			setSlashEntry: &types.SlashEntry{
+				ValidatorAddress:          testtypes.TestValAddr1Str,
+				DelegatorAddress:          testtypes.TestSeqAddr1Str,
+				DelegatorSlashAmount:      sdkmath.OneInt(),
+				DelegatorBondedBalance:    sdkmath.OneInt(), // Set to one to make sure that the hook updates value to 0
+				DelegatorUnbondingBalance: sdkmath.OneInt(), // Set to one to make sure that the hook updates value to 0
+			},
+			height:      height,
+			delAddr:     delegatorAccAddress,
+			valAddr:     validatorAddress,
+			slashAmount: sdkmath.OneInt(),
+			expSlashEntry: types.SlashEntry{
+				ValidatorAddress:          testtypes.TestValAddr1Str,
+				DelegatorAddress:          testtypes.TestSeqAddr1Str,
+				DelegatorSlashAmount:      sdkmath.NewInt(2), // 1 (existing entry) + 1 (new slash amount)
+				DelegatorBondedBalance:    sdkmath.ZeroInt(),
+				DelegatorUnbondingBalance: sdkmath.ZeroInt(),
+			},
+		},
+		{
+			name:        "panics if context height is zero",
+			height:      0,
+			delAddr:     delegatorAccAddress,
+			valAddr:     validatorAddress,
+			slashAmount: sdkmath.OneInt(),
+			expErrMsg:   "slashing height must be positive, received: 0",
+			expPanic:    true,
+		},
+		{
+			name:        "panics if context height is negative",
+			height:      -1,
+			delAddr:     delegatorAccAddress,
+			valAddr:     validatorAddress,
+			slashAmount: sdkmath.OneInt(),
+			expErrMsg:   "slashing height must be positive, received: -1",
+			expPanic:    true,
+		},
+		{
+			name:        "panics if slashed amount is zero",
+			height:      height,
+			delAddr:     delegatorAccAddress,
+			valAddr:     validatorAddress,
+			slashAmount: sdkmath.ZeroInt(),
+			expErrMsg:   "slash amount must be positive, received 0",
+			expPanic:    true,
+		},
+		{
+			name:        "panics if slashed amount is negative",
+			height:      height,
+			delAddr:     delegatorAccAddress,
+			valAddr:     validatorAddress,
+			slashAmount: sdkmath.NewInt(-1),
+			expErrMsg:   "slash amount must be positive, received -1",
+			expPanic:    true,
+		},
+		// The test case below was only written for the sake of completion. We should never be in a situation where a
+		// slash entry is constructed in an invalid manner. In fact to trigger this case we will be storing a negative
+		// slash amount in state, which should never occur if we call validate basic before storing.
+		{
+			name: "panics if constructed invalid slash entry",
+			setSlashEntry: &types.SlashEntry{
+				ValidatorAddress: testtypes.TestValAddr1Str,
+				DelegatorAddress: testtypes.TestSeqAddr1Str,
+
+				// Set to a value > slash amount so resultant slash entry has negative slash amount
+				DelegatorSlashAmount: sdkmath.NewInt(-10),
+
+				DelegatorBondedBalance:    sdkmath.ZeroInt(),
+				DelegatorUnbondingBalance: sdkmath.ZeroInt(),
+			},
+			height:      height,
+			delAddr:     delegatorAccAddress,
+			valAddr:     validatorAddress,
+			slashAmount: sdkmath.NewInt(-1),
+			expErrMsg:   "AfterUnbondingDelegationSlashed: constructed invalid slash entry",
+			expPanic:    true,
+		},
+	}
+
+	for _, tc := range testCases {
+		s.Run(tc.name, func() {
+			s.SetupTest()
+
+			// Update context's height as required by test
+			ctxWithHeight := s.Ctx().WithBlockHeight(tc.height)
+
+			// Set slash entry if required by test
+			if tc.setSlashEntry != nil {
+				s.App.ReportsKeeper.SetSlashEntry(ctxWithHeight, uint64(tc.height), *tc.setSlashEntry)
+			}
+
+			// Execute hook
+			if tc.expPanic {
+
+				// Expect a panic if required by test
+				s.Require().Panics(func() {
+					defer func() {
+						if r := recover(); r != nil {
+							s.Require().Contains(r.(string), tc.expErrMsg)
+						}
+					}()
+
+					err := s.App.ReportsKeeper.Hooks().AfterUnbondingDelegationSlashed(
+						ctxWithHeight, tc.valAddr, tc.delAddr, tc.slashAmount,
+					)
+					s.Require().Fail(fmt.Sprintf("Expected panic but got error: %v", err))
+				})
+			} else {
+
+				// If no panic is expected, run the hook without wrapping a deferred function and check for errors or
+				// the execution results, as required by the test case/
+				err := s.App.ReportsKeeper.Hooks().AfterUnbondingDelegationSlashed(
+					ctxWithHeight, tc.valAddr, tc.delAddr, tc.slashAmount,
+				)
+				if len(tc.expErrMsg) > 0 {
+					s.Require().Error(err)
+					s.Require().ErrorContains(err, tc.expErrMsg)
+					return
+				}
+				s.Require().NoError(err)
+
+				actualSlashEntry, found := s.App.ReportsKeeper.GetSlashEntry(
+					ctxWithHeight, uint64(tc.height), tc.delAddr.String(), tc.valAddr.String(),
+				)
+				s.Require().True(found)
+				s.Require().Equal(tc.expSlashEntry, actualSlashEntry)
+			}
+		})
+	}
+}
