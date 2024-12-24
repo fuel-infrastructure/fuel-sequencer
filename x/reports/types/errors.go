bd026859b4571454f0605cee4713a4b8080d415e
migueldingli1997 <migueldingli97@gmail.com>
2024-12-24 14:44:33 +0100

    Add pruning of slash reports
    Signed-off-by: migueldingli1997 <migueldingli97@gmail.com>

--- x/reports/types/errors.go
+++ x/reports/types/errors.go
@@ -11,4 +11,5 @@ var (
 	ErrInvalidSigner        = sdkerrors.Register(ModuleName, 1100, "invalid signer")
 	ErrSlashReportNotUnique = sdkerrors.Register(ModuleName, 1101, "duplicate slash report found")
 	ErrSlashEntryNotUnique  = sdkerrors.Register(ModuleName, 1102, "duplicate slash entry found")
+	ErrParamsInvalid        = sdkerrors.Register(ModuleName, 1103, "params are invalid")
 )
--- x/reports/types/params.go
+++ x/reports/types/params.go
@@ -6,14 +6,24 @@ import (
 
 var _ paramtypes.ParamSet = (*Params)(nil)
 
+const (
+	DefaultMaxSlashReportAgeBlocks = 14400
+)
+
 // NewParams creates a new Params instance
-func NewParams() Params {
-	return Params{}
+func NewParams(
+	maxSlashReportAgeBlocks uint64,
+) Params {
+	return Params{
+		MaxSlashReportAgeBlocks: maxSlashReportAgeBlocks,
+	}
 }
 
 // DefaultParams returns a default set of parameters
 func DefaultParams() Params {
-	return NewParams()
+	return NewParams(
+		DefaultMaxSlashReportAgeBlocks,
+	)
 }
 
 // ParamSetPairs get the params.ParamSet
@@ -25,5 +35,21 @@ func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
 
 // Validate validates the set of params
 func (p Params) Validate() error {
+
+	if err := ValidateMaxSlashReportAgeBlocks(p.MaxSlashReportAgeBlocks); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ValidateMaxSlashReportAgeBlocks(i interface{}) error {
+	v, ok := i.(uint64)
+	if !ok {
+		return ErrParamsInvalid.Wrapf("invalid parameter type: %T", i)
+	}
+	if v <= 0 {
+		return ErrParamsInvalid.Wrapf("max slash report age blocks must be positive, got: %d", v)
+	}
 	return nil
 }
