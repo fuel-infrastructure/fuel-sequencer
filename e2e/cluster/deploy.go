cb80f39a527c30a1d848adedfc84b1a65600747e
Tharen Abela <tharen@simplystaking.com>
2025-01-03 18:36:01 +0000

    minor: e2e/cluster: remotely cmd with session
    - convenience wrapper for each command executed remotely

--- e2e/cluster/deploy.go
+++ e2e/cluster/deploy.go
@@ -22,16 +22,9 @@ func deployNetwork(connections []connection, binaryPath string) error {
 func deployNode(l *zap.SugaredLogger, conn connection, binaryName string) error {
 	remotePath := filepath.Join(conn.dir, binaryName)
 
-	session, err := conn.SSH.NewSession()
-	if err != nil {
-		l.Errorw("failed to create ssh session", "error", err)
-		return fmt.Errorf("failed to create session: %w", err)
-	}
-	defer session.Close()
-
 	// Start the node with appropriate configuration
 	cmd := fmt.Sprintf("%s --home %s start", remotePath, homeDir(conn.destination))
-	if err := remotely(l, session, cmd); err != nil {
+	if err := remotely(l, conn.SSH, cmd); err != nil {
 		return fmt.Errorf("failed to start node: %w", err)
 	}
 
