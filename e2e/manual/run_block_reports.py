340356ac64f65bc77f27ac379158345525f5c9b5
Tharen Abela <tharen@simplystaking.com>
2025-01-06 18:11:34 +0000

    e2e/manual: reports limited to running module
    - prevents running unnecessary code if block reports are just imported
    - if block reports run directly, should be same as before.

--- e2e/manual/run_block_reports.py
+++ e2e/manual/run_block_reports.py
@@ -8,7 +8,6 @@ from utils.networks import NetworkConfig, Networks
 
 NETWORK = Networks.TESTNET  # Change me to load test other networks!
 CONFIG = NetworkConfig(NETWORK)
-print(f"Running block reports on {NETWORK}")
 
 sequencer_rpc = CONFIG.seq_rpc
 sequencer_rest = CONFIG.seq_rest
@@ -39,32 +38,6 @@ def get_latest_commit():
 
 def get_latest_block():
     return get_block("")
-
-
-# Calculate block range based on latest height
-latest_block = get_latest_commit()
-latest_height = int(latest_block["signed_header"]["header"]["height"])
-end_heights = [latest_height]
-start_heights = [latest_height - 200]
-
-# Optional: manual override of heights
-# start_heights = []
-# end_heights = []
-
-# Get number of transactions in each block range for these signers
-signers = [
-    # "fuelsequencer1vtfzrk6f4m6kxt6ehyqt9j5su5hvcz5q3dmlsm",  # alice
-    # "fuelsequencer1ptrdx8rzykw57suy540tlhfhxfclmykywhtycf",  # temp1
-    # "fuelsequencer1z6mj9pgyd0gdt7zpnn5k5xgw3u99ammylkh0tn",  # temp2
-    # "fuelsequencer18q22y7cyxsev5ttqkvqt6dse0zd6zejyqja2h0",  # temp3
-    # "fuelsequencer1wnfm34d2wees97cl34qcev8de3ja2n8whh732u",  # temp4
-    # "fuelsequencer1h4dr6td79wagkgt5z8qhzaazh30nnavgq8g7wf",  # temp5
-    # "fuelsequencer1w683zjxx9pvnceakafaf3penc060uhggdqjsw3",  # temp6
-    # "fuelsequencer16eqy4fjtrr5rf8e0gfsc48qlzdpfl5enng849y",  # temp7
-    # "fuelsequencer1ngsagnggmmumc62220n6waav5wzjgm8hf8kzh0",  # temp8
-    # "fuelsequencer1cyvqtp695llvpcu08xg5ndjsqre0nsash972vz",  # temp9
-    # "fuelsequencer1lut8dr0473pxm9ayynay7hhya9n8dnsa6egh5p",  # temp10
-]
 
 
 def base64_decoded_size(encoded_str):
@@ -121,18 +94,46 @@ def run_report_3(start_height: int, end_height: int):
         time.sleep(1)
 
 
-# Report 1
-for i in range(len(start_heights)):
-    print(f"\n--- (Report 1.{i})")
-    run_report_1(start_heights[i], end_heights[i])
+if __name__ == "__main__":
+    print(f"Running block reports on {NETWORK}")
 
-# Report 2
-for i, signer in enumerate(signers):
-    print(f"\n--- (Report 2.{i})")
-    for j in range(len(start_heights)):
-        run_report_2(start_heights[j], end_heights[j], signer)
+    # Calculate block range based on latest height
+    latest_block = get_latest_commit()
+    latest_height = int(latest_block["signed_header"]["header"]["height"])
+    end_heights = [latest_height]
+    start_heights = [latest_height - 200]
 
-# Report 3
-for i in range(len(start_heights)):
-    print(f"\n--- (Report 3.{i})")
-    run_report_3(start_heights[i], end_heights[i])
+    # Optional: manual override of heights
+    # start_heights = []
+    # end_heights = []
+
+    # Get number of transactions in each block range for these signers
+    signers = [
+        # "fuelsequencer1vtfzrk6f4m6kxt6ehyqt9j5su5hvcz5q3dmlsm",  # alice
+        # "fuelsequencer1ptrdx8rzykw57suy540tlhfhxfclmykywhtycf",  # temp1
+        # "fuelsequencer1z6mj9pgyd0gdt7zpnn5k5xgw3u99ammylkh0tn",  # temp2
+        # "fuelsequencer18q22y7cyxsev5ttqkvqt6dse0zd6zejyqja2h0",  # temp3
+        # "fuelsequencer1wnfm34d2wees97cl34qcev8de3ja2n8whh732u",  # temp4
+        # "fuelsequencer1h4dr6td79wagkgt5z8qhzaazh30nnavgq8g7wf",  # temp5
+        # "fuelsequencer1w683zjxx9pvnceakafaf3penc060uhggdqjsw3",  # temp6
+        # "fuelsequencer16eqy4fjtrr5rf8e0gfsc48qlzdpfl5enng849y",  # temp7
+        # "fuelsequencer1ngsagnggmmumc62220n6waav5wzjgm8hf8kzh0",  # temp8
+        # "fuelsequencer1cyvqtp695llvpcu08xg5ndjsqre0nsash972vz",  # temp9
+        # "fuelsequencer1lut8dr0473pxm9ayynay7hhya9n8dnsa6egh5p",  # temp10
+    ]
+
+    # Report 1
+    for i in range(len(start_heights)):
+        print(f"\n--- (Report 1.{i})")
+        run_report_1(start_heights[i], end_heights[i])
+
+    # Report 2
+    for i, signer in enumerate(signers):
+        print(f"\n--- (Report 2.{i})")
+        for j in range(len(start_heights)):
+            run_report_2(start_heights[j], end_heights[j], signer)
+
+    # Report 3
+    for i in range(len(start_heights)):
+        print(f"\n--- (Report 3.{i})")
+        run_report_3(start_heights[i], end_heights[i])
