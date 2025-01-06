1dec07afd9264b05f677aac3036d99c15904a5e0
Tharen Abela <tharen@simplystaking.com>
2025-01-06 12:13:05 +0000

    minor: e2e/manual: formatting

--- e2e/manual/load_test/utils.py
+++ e2e/manual/load_test/utils.py
@@ -15,32 +15,25 @@ def get_temp_tx_file_prefix(key_name: str):
     return f"temp.{key_name}"
 
 
-def get_temp_tx_files(
-        key_name: str,
-        rollup: int,
-        order: int
-) -> Tuple[str, str]:
+def get_temp_tx_files(key_name: str, rollup: int, order: int) -> Tuple[str, str]:
     filename_prefix = get_temp_tx_file_prefix(key_name)
     tx_file_path = os.path.join(
         get_temp_txs_folder(),
-        f"{filename_prefix}.{rollup}.{order % MAX_TEMP_FILES}.json"
+        f"{filename_prefix}.{rollup}.{order % MAX_TEMP_FILES}.json",
     )
     result_file_path = os.path.join(
         get_temp_txs_folder(),
-        f"{filename_prefix}.{rollup}.{order % MAX_TEMP_FILES}-result.json"
+        f"{filename_prefix}.{rollup}.{order % MAX_TEMP_FILES}-result.json",
     )
     return tx_file_path, result_file_path
 
 
-def get_logger(
-        name: str,
-        logging_queue: multiprocessing.Queue
-) -> logging.Logger:
+def get_logger(name: str, logging_queue: multiprocessing.Queue) -> logging.Logger:
     logger = logging.getLogger(name)
     handler = logging.handlers.QueueHandler(logging_queue)
     formatter = logging.Formatter(
-        '%(asctime)s %(levelname)-6s %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
+        "%(asctime)s %(levelname)-6s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
     handler.setFormatter(formatter)
     logger.addHandler(handler)
@@ -50,8 +43,8 @@ def get_logger(
 
 def get_tx_info_from_tx_result(result: Dict, explorer_tx_url: str) -> str:
     try:
-        if result['code'] != 0:
-            return result['raw_log']
+        if result["code"] != 0:
+            return result["raw_log"]
         else:
             return f"{explorer_tx_url}{result['txhash']}"
     except Exception:
