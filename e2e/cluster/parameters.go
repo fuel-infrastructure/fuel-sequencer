0f95ebef609fe91a92daafaa85b635832182eba2
Tharen Abela <tharen@simplystaking.com>
2025-01-10 09:21:26 +0100

    minor: e2e/cluster: parameters dropped type hints

--- e2e/cluster/parameters.go
+++ e2e/cluster/parameters.go
@@ -18,13 +18,13 @@ const (
 
 var (
 	// Binary Parameters
-	makefileDir string = "/home/user/fuel-sequencer"                                 // Absolute path to the directory where makefile is located
-	wantArch    string = "linux-amd64"                                               // Arch specified from build binary suffix
-	buildPath   string = makefileDir + "/build"                                      // Path where binary will be built
-	dataDir     string = makefileDir + "/e2e/cluster/data"                           // Directory with template data
-	servicePath string = makefileDir + "/e2e/cluster/systemd/fuelsequencerd.service" // Path to the systemd service file
-	systemdPath string = "/etc/systemd/system/fuelsequencerd.service"                // Path to the systemd service file on the remote machine
-	binaryName  string = "fuelsequencerd"                                            // Name of the binary
+	makefileDir = "/home/user/fuel-sequencer"                                 // Absolute path to the directory where makefile is located
+	wantArch    = "linux-amd64"                                               // Arch specified from build binary suffix
+	buildPath   = makefileDir + "/build"                                      // Path where binary will be built
+	dataDir     = makefileDir + "/e2e/cluster/data"                           // Directory with template data
+	servicePath = makefileDir + "/e2e/cluster/systemd/fuelsequencerd.service" // Path to the systemd service file
+	systemdPath = "/etc/systemd/system/fuelsequencerd.service"                // Path to the systemd service file on the remote machine
+	binaryName  = "fuelsequencerd"                                            // Name of the binary
 
 	// Sequencer Parameters
 	chainName = "seq-benchnet-1"
