e6922b8ee5b1474329cca7239c41ead29e5048ac
Miguel Dingli <migueldingli97@gmail.com>
2025-01-20 10:41:16 +0100

    Update sidecar/ethwrappedclient/rpc_client.go
    Co-authored-by: dillu24 <dylangalea96@gmail.com>

--- sidecar/ethwrappedclient/rpc_client.go
+++ sidecar/ethwrappedclient/rpc_client.go
@@ -29,7 +29,7 @@ type EthRpcClient struct {
 	contractABI abi.ABI
 
 	// seqBridgeDenom is the denom used when encoding AuthorizeTx messages.
-	// For example, if we observe a DelegateEvent with amount 100, this is translated to a MsgDelegate of 100ufuel.
+	// For example, if we observe a DelegateEvent with amount 100, this is translated to a MsgDelegate of 100seqBridgeDenom.
 	seqBridgeDenom string
 
 	// logsQueryLimiter limits how many queries for logs we can perform in a time interval.
