d74032f08537b823e0dfb9b152ac58672247ff35
Tharen Abela <tharen@simplystaking.com>
2025-02-07 11:55:38 +0100

    e2e: event tests restored without batchAuthorize

--- e2e/tests/events/event_trimming_test.go
+++ e2e/tests/events/event_trimming_test.go
@@ -1,196 +1,159 @@
 package events_test
 
-// TODO: These tests are no longer valid now. Need to find ways to generate multiple events in a single Ethereum block.
+import (
+	"fmt"
+	"math/big"
+	"time"
+
+	sdkmath "cosmossdk.io/math"
+	cmtypes "github.com/cometbft/cometbft/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	consensustypes "github.com/cosmos/cosmos-sdk/x/consensus/types"
+	"github.com/fuel-infrastructure/fuel-sequencer/e2e/testsuite"
+	"github.com/fuel-infrastructure/fuel-sequencer/sidecar/service/types"
+	"github.com/fuel-infrastructure/fuel-sequencer/utils"
+	bridgetypes "github.com/fuel-infrastructure/fuel-sequencer/x/bridge/types"
+)
 
 // TestEventTrimming sets a reduced max bytes for blocks to showcase event trimming.
-// func (s *EventsTestSuite) TestEventTrimming() {
+func (s *EventsTestSuite) TestEventTrimming() {
 
-// 	s.Run("Run with reduced max bytes to showcase event trimming", func() {
+	s.Run("Run with reduced max bytes to showcase event trimming", func() {
 
-// 		// All transactions will have a non-zero sequence
-// 		nonZeroSequence := uint64(1)
+		// All transactions will have a non-zero sequence
+		nonZeroSequence := uint64(1)
 
-// 		// Calculate size of transaction resulting from MsgIndex.
-// 		typicalMsgIndex := &bridgetypes.MsgIndex{
-// 			Authority:           s.GetGovernanceAddress(),
-// 			NumInjectedEventTxs: 1, // matches the number of events emitted by AuthorizeMulti, per sequencer block
-// 			NewEthereumBlock:    false,
-// 			BlockNumber:         1,
-// 		}
-// 		typicalMsgIndexBz, err := typicalMsgIndex.RawTxBytes(nonZeroSequence)
-// 		s.Require().NoError(err)
-// 		typicalMsgIndexSize := utils.TxSize(typicalMsgIndexBz)
+		// Calculate size of transaction resulting from MsgIndex.
+		typicalMsgIndex := &bridgetypes.MsgIndex{
+			Authority:           s.GetGovernanceAddress(),
+			NumInjectedEventTxs: 1,
+			NewEthereumBlock:    false,
+			BlockNumber:         1,
+		}
+		typicalMsgIndexBz, err := typicalMsgIndex.RawTxBytes(nonZeroSequence)
+		s.Require().NoError(err)
+		typicalMsgIndexSize := utils.TxSize(typicalMsgIndexBz)
 
-// 		s.Logger().Info(fmt.Sprintf("Predicted size of MsgIndex: %d", typicalMsgIndexSize))
+		s.Logger().Info(fmt.Sprintf("Predicted size of MsgIndex: %d", typicalMsgIndexSize))
 
-// 		// Generate a Transfer
-// 		sendAmount := int64(10)
-// 		from := s.EthKeys[0]
-// 		to := s.EthKeys[1]
-// 		msgSend := testsuite.PackTransfer(to.Address, big.NewInt(sendAmount))
-// 		msgSendBz := s.E2ETestSuite.GenerateMsgSendBz(
-// 			from.AddressHex, to.AddressHex, sdk.NewCoins(sdk.NewCoin(testsuite.BridgeDenom, sdkmath.NewInt(sendAmount))),
-// 		)
+		// Generate a Transfer
+		sendAmount := int64(10)
+		sendCoin := sdk.NewCoin(testsuite.BridgeDenom, sdkmath.NewInt(sendAmount))
+		from := s.EthKeys[0]
+		validator := s.SeqKeys[0]
 
-// 		// Calculate size of transaction resulting from AuthorizeEvent.
-// 		authorizeEvent := types.AuthorizeEvent{
-// 			Sender: from.AddressHex,
-// 			Data:   msgSendBz,
-// 		}
-// 		authorizeEventMsg, err := authorizeEvent.Messages(testsuite.TestCdc, s.GetGovernanceAddress())
-// 		s.Require().NoError(err)
-// 		authorizeEventMsgBz, err := utils.ValidRawTxBytesFromAnyMsgs(authorizeEventMsg, nonZeroSequence)
-// 		s.Require().NoError(err)
-// 		authorizeEventMsgSize := utils.TxSize(authorizeEventMsgBz)
+		msgDelegateBz := s.E2ETestSuite.GenerateMsgDelegateBz(from.AddressHex, validator.ValAddressHex, sendCoin)
 
-// 		s.Logger().Info(fmt.Sprintf("Predicted size of tx from AuthorizeEvent: %d", authorizeEventMsgSize))
+		// Generate a deposit & delegate
+		depositEvent := types.DepositEvent{
+			Depositor: from.AddressHex,
+			Recipient: from.AddressHex,
+			Amount:    sendCoin.Amount.String(),
+			Lockup:    "0",
+		}
+		delegateEvent := types.AuthorizeEvent{
+			Sender: from.AddressHex,
+			Data:   msgDelegateBz,
+		}
 
-// 		// Set a low max bytes for txs so that events are split across multiple blocks, with a buffer of 10 bytes.
-// 		maxBytesForTransactions := int64(typicalMsgIndexSize + authorizeEventMsgSize + 10)
+		// Calculate size of transaction resulting from AuthorizeEvent.
+		depositEventMsg, err := depositEvent.Messages(testsuite.TestCdc, s.GetGovernanceAddress())
+		s.Require().NoError(err)
+		depositEventMsgBz, err := utils.ValidRawTxBytesFromAnyMsgs(depositEventMsg, nonZeroSequence)
+		s.Require().NoError(err)
+		depositEventMsgSize := utils.TxSize(depositEventMsgBz)
+		s.Logger().Info(fmt.Sprintf("Predicted size of tx from DepositEvent: %d", depositEventMsgSize))
 
-// 		// Calculate a max block size - this is not just for txs and must consider
-// 		// the max size of the header and other components that make up a block.
-// 		numberOfValidators := len(testsuite.MNEMONICS)
-// 		maxBytes := maxBytesForTransactions +
-// 			cmtypes.MaxOverheadForBlock +
-// 			cmtypes.MaxHeaderBytes +
-// 			cmtypes.MaxCommitBytes(numberOfValidators)
-// 		s.Require().NotPanics(func() {
-// 			_ = cmtypes.MaxDataBytesNoEvidence(maxBytes, numberOfValidators)
-// 		})
+		delegateEventMsg, err := delegateEvent.Messages(testsuite.TestCdc, s.GetGovernanceAddress())
+		s.Require().NoError(err)
+		delegateEventMsgBz, err := utils.ValidRawTxBytesFromAnyMsgs(delegateEventMsg, nonZeroSequence)
+		s.Require().NoError(err)
+		delegateEventMsgSize := utils.TxSize(delegateEventMsgBz)
+		s.Logger().Info(fmt.Sprintf("Predicted size of tx from DelegateEvent: %d", delegateEventMsgSize))
 
-// 		consensusParams := s.QueryConsensusParams(s.Ctx())
-// 		consensusParams.Evidence.MaxBytes = 1
-// 		consensusParams.Block.MaxBytes = maxBytes
+		// Set a low max bytes for txs so that events are split across multiple blocks, with a buffer of 10 bytes.
+		largestEventMsgSize := depositEventMsgSize
+		if delegateEventMsgSize > depositEventMsgSize {
+			largestEventMsgSize = delegateEventMsgSize
+		}
+		maxBytesForTransactions := int64(typicalMsgIndexSize + largestEventMsgSize + 10)
 
-// 		msgUpdateParams := consensustypes.MsgUpdateParams{
-// 			Authority: s.GetGovernanceAddress(),
-// 			Block:     consensusParams.Block,
-// 			Evidence:  consensusParams.Evidence,
-// 			Validator: consensusParams.Validator,
-// 			Abci:      consensusParams.Abci,
-// 		}
-// 		s.ExecuteGovProposal(&msgUpdateParams)
+		// Calculate a max block size - this is not just for txs and must consider
+		// the max size of the header and other components that make up a block.
+		numberOfValidators := len(testsuite.MNEMONICS)
+		maxBytes := maxBytesForTransactions +
+			cmtypes.MaxOverheadForBlock +
+			cmtypes.MaxHeaderBytes +
+			cmtypes.MaxCommitBytes(numberOfValidators)
+		s.Require().NotPanics(func() {
+			_ = cmtypes.MaxDataBytesNoEvidence(maxBytes, numberOfValidators)
+		})
 
-// 		// Check max block size was updated
-// 		consensusParams = s.QueryConsensusParams(s.Ctx())
-// 		s.Require().EqualValues(maxBytes, consensusParams.Block.MaxBytes)
+		consensusParams := s.QueryConsensusParams(s.Ctx())
+		consensusParams.Evidence.MaxBytes = 1
+		consensusParams.Block.MaxBytes = maxBytes
 
-// 		// Try generating some events via a transaction (RPC) - i = number of events
-// 		for i := 0; i < 4; i++ {
-// 			_, err := s.SendEthTransactionToSequencerInterfaceContract(msgSend)
-// 			s.Require().NoError(err, fmt.Errorf("error while sending transaction no.%d: err: %w", i+1, err))
-// 		}
+		msgUpdateParams := consensustypes.MsgUpdateParams{
+			Authority: s.GetGovernanceAddress(),
+			Block:     consensusParams.Block,
+			Evidence:  consensusParams.Evidence,
+			Validator: consensusParams.Validator,
+			Abci:      consensusParams.Abci,
+		}
+		s.ExecuteGovProposal(&msgUpdateParams)
 
-// 		// 1st event of 4 processed
-// 		s.PollForEthereumEventIndexOffset(s.Ctx(), 20, 0)
-// 		// 2nd event of 4 processed
-// 		s.PollForEthereumEventIndexOffset(s.Ctx(), 2, 0)
-// 		// 3rd event of 4 processed
-// 		s.PollForEthereumEventIndexOffset(s.Ctx(), 2, 0)
-// 		// 4th event of 4 processed
-// 		s.PollForEthereumEventIndexOffset(s.Ctx(), 2, 0)
-// 	})
-// }
+		// Check max block size was updated
+		consensusParams = s.QueryConsensusParams(s.Ctx())
+		s.Require().EqualValues(maxBytes, consensusParams.Block.MaxBytes)
 
-// func (s *EventsTestSuite) TestMaxEthBlockUpdateDelay() {
+		// Send deposit and delegate events,
+		_ = s.DepositAndDelegateTokenToSequencer(sendCoin.Amount.BigInt(), validator.ValAddressEth)
+		s.PollForEthereumEventIndexOffset(s.Ctx(), 20, 1) // check for deposit event processed
+		s.PollForEthereumEventIndexOffset(s.Ctx(), 2, 0)  // check for delegate event processed in the next block
+	})
+}
 
-// 	s.Run("Test validators can resume the chain if block is too large and max syncup delay exceeded", func() {
+func (s *EventsTestSuite) TestMaxEthBlockUpdateDelay() {
 
-// 		// -------- Setup
+	s.Run("Test validators can resume the chain if block is too large and max syncup delay exceeded", func() {
 
-// 		// All transactions will have a non-zero sequence
-// 		nonZeroSequence := uint64(1)
+		// -------- Setup
 
-// 		// Calculate size of transaction resulting from MsgIndex.
-// 		typicalMsgIndex := &bridgetypes.MsgIndex{
-// 			Authority:           s.GetGovernanceAddress(),
-// 			NumInjectedEventTxs: 1, // matches the number of events emitted by AuthorizeMulti, per sequencer block
-// 			NewEthereumBlock:    false,
-// 			BlockNumber:         1,
-// 		}
-// 		typicalMsgIndexBz, err := typicalMsgIndex.RawTxBytes(nonZeroSequence)
-// 		s.Require().NoError(err)
-// 		typicalMsgIndexSize := utils.TxSize(typicalMsgIndexBz)
+		// All transactions will have a non-zero sequence
+		nonZeroSequence := uint64(1)
 
-// 		s.Logger().Info(fmt.Sprintf("Predicted size of MsgIndex: %d", typicalMsgIndexSize))
+		// Calculate size of transaction resulting from MsgIndex.
+		typicalMsgIndex := &bridgetypes.MsgIndex{
+			Authority:           s.GetGovernanceAddress(),
+			NumInjectedEventTxs: 1, // matches the number of events emitted by AuthorizeMulti, per sequencer block
+			NewEthereumBlock:    false,
+			BlockNumber:         1,
+		}
+		typicalMsgIndexBz, err := typicalMsgIndex.RawTxBytes(nonZeroSequence)
+		s.Require().NoError(err)
+		typicalMsgIndexSize := utils.TxSize(typicalMsgIndexBz)
 
-// 		// Generate a Transfer
-// 		sendAmount := int64(10)
-// 		from := s.EthKeys[0]
-// 		to := s.EthKeys[1]
-// 		msgSend := testsuite.PackTransfer(to.Address, big.NewInt(sendAmount))
-// 		msgSendBz := s.E2ETestSuite.GenerateMsgSendBz(
-// 			from.AddressHex, to.AddressHex, sdk.NewCoins(sdk.NewCoin(testsuite.BridgeDenom, sdkmath.NewInt(sendAmount))),
-// 		)
+		s.Logger().Info(fmt.Sprintf("Predicted size of MsgIndex: %d", typicalMsgIndexSize))
 
-// 		// Calculate size of transaction resulting from AuthorizeEvent.
-// 		authorizeEvent := types.AuthorizeEvent{
-// 			Sender: from.AddressHex,
-// 			Data:   msgSendBz,
-// 		}
-// 		authorizeEventMsg, err := authorizeEvent.Messages(testsuite.TestCdc, s.GetGovernanceAddress())
-// 		s.Require().NoError(err)
-// 		authorizeEventMsgBz, err := utils.ValidRawTxBytesFromAnyMsgs(authorizeEventMsg, nonZeroSequence)
-// 		s.Require().NoError(err)
-// 		authorizeEventMsgSize := utils.TxSize(authorizeEventMsgBz)
+		// Generate a Transfer
+		sendAmount := int64(10)
+		to := s.EthKeys[1].Address
+		msgSend := testsuite.PackTransfer(to, big.NewInt(sendAmount))
 
-// 		s.Logger().Info(fmt.Sprintf("Predicted size of tx from AuthorizeEvent: %d", authorizeEventMsgSize))
+		// -------- Send transactions
 
-// 		// Set a low max bytes for txs so that events are split across multiple blocks, with a buffer of 10 bytes.
-// 		maxBytesForTransactions := int64(typicalMsgIndexSize + authorizeEventMsgSize + 10)
+		txReceipt, err := s.SendEthTransactionToSequencerInterfaceContract(msgSend)
+		s.Require().NoError(err)
 
-// 		// Calculate max block size - this is not just for txs and must consider
-// 		// the max size of the header and other components that make up a block.
-// 		numberOfValidators := len(testsuite.MNEMONICS)
-// 		maxBytes := maxBytesForTransactions +
-// 			cmtypes.MaxOverheadForBlock +
-// 			cmtypes.MaxHeaderBytes +
-// 			cmtypes.MaxCommitBytes(numberOfValidators)
-// 		s.Require().NotPanics(func() {
-// 			_ = cmtypes.MaxDataBytesNoEvidence(maxBytes, numberOfValidators)
-// 		})
+		// -------- Delay sync up
 
-// 		consensusParams := s.QueryConsensusParams(s.Ctx())
-// 		consensusParams.Evidence.MaxBytes = 1
-// 		consensusParams.Block.MaxBytes = maxBytes
+		s.PauseEthereum()
+		// Wait MaxEthBlockUpdateDelay is no longer valid (padded some seconds due to caching on the sidecars)
+		time.Sleep(time.Second * 40)
+		s.UnpauseEthereum()
 
-// 		msgUpdateParams := consensustypes.MsgUpdateParams{
-// 			Authority: s.GetGovernanceAddress(),
-// 			Block:     consensusParams.Block,
-// 			Evidence:  consensusParams.Evidence,
-// 			Validator: consensusParams.Validator,
-// 			Abci:      consensusParams.Abci,
-// 		}
-// 		s.ExecuteGovProposal(&msgUpdateParams)
+		// -------- Check that events are eventually processed
 
-// 		// Check max block size was updated
-// 		consensusParams = s.QueryConsensusParams(s.Ctx())
-// 		s.Require().EqualValues(maxBytes, consensusParams.Block.MaxBytes)
-
-// 		// -------- Send transactions
-
-// 		for i := 0; i < 4; i++ {
-// 			_, err := s.SendEthTransactionToSequencerInterfaceContract(msgSend)
-// 			s.Require().NoError(err, fmt.Errorf("error while sending transaction no.%d: err: %w", i+1, err))
-// 		}
-
-// 		// -------- Delay sync up
-
-// 		s.PauseEthereum()
-// 		// Wait MaxEthBlockUpdateDelay is no longer valid (padded some seconds due to caching on the sidecars)
-// 		time.Sleep(time.Second * 40)
-// 		s.UnpauseEthereum()
-
-// 		// -------- Check that events are eventually processed
-
-// 		// 1st event of 4 processed
-// 		s.PollForEthereumEventIndexOffset(s.Ctx(), 20, 0)
-// 		// 2nd event of 4 processed
-// 		s.PollForEthereumEventIndexOffset(s.Ctx(), 2, 0)
-// 		// 3rd event of 4 processed
-// 		s.PollForEthereumEventIndexOffset(s.Ctx(), 2, 0)
-// 		// 4th event of 4 processed
-// 		s.PollForEthereumEventIndexOffset(s.Ctx(), 2, 0)
-// 	})
-// }
+		s.PollForLastEthereumBlockSynced(s.Ctx(), 20, txReceipt.BlockNumber.Uint64())
+	})
+}
