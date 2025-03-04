f1360788dd041e45c26aff97e47511a0ffde9155
Tharen Abela <tharen@simplystaking.com>
2025-03-04 20:52:48 +0400

    bridge: skip_event_tx with ValidateBasic

--- x/bridge/types/message_skip_event_tx.go
+++ x/bridge/types/message_skip_event_tx.go
@@ -19,6 +19,12 @@ func NewMsgSkippedEventTx(authority string, reasonForSkip string, ethBlockNumber
 	}
 }
 
+// ValidateBasic for this message should be a no-op so that we definitely AnteHandle this message.
+// Since we generate the MsgSkippedEventTx ourselves, we expect the message to be valid anyway.
+func (*MsgSkippedEventTx) ValidateBasic() error {
+	return nil
+}
+
 // RawTxBytes converts the message to a valid tx that can be injected into a block and produces a tx result.
 // The sequence, presumed to be unique, ensures that the generated tx is unique and thus has a unique tx hash.
 func (m *MsgSkippedEventTx) RawTxBytes(sequence uint64) ([]byte, error) {
