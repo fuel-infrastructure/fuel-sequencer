0bc9ab13229b1f928c14c038583762e12b3a100f
Miguel Dingli <migueldingli97@gmail.com>
2025-03-06 17:40:01 +0000

    Get tests to pass
    Signed-off-by: Miguel Dingli <migueldingli97@gmail.com>

--- e2e/testsuite/query_commitments.go
+++ e2e/testsuite/query_commitments.go
@@ -6,8 +6,40 @@ import (
 
 	"github.com/cometbft/cometbft/crypto/merkle"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/fuel-infrastructure/fuel-sequencer/utils"
 	commitmentstypes "github.com/fuel-infrastructure/fuel-sequencer/x/commitments/types"
 )
+
+// publicValues is a set of values required when calling updateCommitHeaderRange.
+type publicValues struct {
+	TrustedBlock      uint64
+	TrustedHeaderHash common.Hash
+	TargetBlock       uint64
+	TargetHeaderHash  common.Hash
+	BridgeCommitment  common.Hash
+}
+
+func (pv *publicValues) paddedBytes() ([]byte, error) {
+	var bz []byte
+
+	trustedBlockBz, err := utils.To32PaddedHexBytes(pv.TrustedBlock)
+	if err != nil {
+		return nil, err
+	}
+
+	targetBlockBz, err := utils.To32PaddedHexBytes(pv.TargetBlock)
+	if err != nil {
+		return nil, err
+	}
+
+	bz = append(bz, trustedBlockBz...)
+	bz = append(bz, pv.TrustedHeaderHash.Bytes()...)
+	bz = append(bz, targetBlockBz...)
+	bz = append(bz, pv.TargetHeaderHash.Bytes()...)
+	bz = append(bz, pv.BridgeCommitment.Bytes()...)
+
+	return bz, nil
+}
 
 // AuntsToHashes takes aunts from a Merkle proof and converts them to 32-byte hashes.
 func AuntsToHashes(proof merkle.Proof) (hashes []common.Hash) {
@@ -48,18 +80,44 @@ func (s *E2ETestSuite) QueryBridgeCommitmentInclusionProof(
 	return res
 }
 
+// GetDataForUpdateCommitHeaderRange generates the data needed for the updateCommitHeaderRange function to pass.
+//
+// It includes an option to get the trusted header hash from the contract, since we do not have permission to set the
+// initial trusted header, since it's reserved to the contract admin. Thankfully it's not that important to test this.
 func (s *E2ETestSuite) GetDataForUpdateCommitHeaderRange(
-	ctx context.Context, start, end uint64,
-) (targetHeaderHash, bridgeCommitmentHash common.Hash) {
-
-	block, err := s.GetBlockByHeight(ctx, int64(end))
-	s.Require().NoError(err)
-	targetHeaderHash = common.BytesToHash(block.LastCommit.BlockID.Hash)
+	ctx context.Context, start, end uint64, trustedHeaderHashFromContract bool,
+) (proof, publicValuesBz []byte) {
 
 	bridgeCommitment := s.QueryBridgeCommitment(ctx, start, end)
-	bridgeCommitmentHash = common.BytesToHash(bridgeCommitment)
+	bridgeCommitmentHash := common.BytesToHash(bridgeCommitment)
 
-	return
+	var trustedHeaderHash common.Hash
+	var err error
+	if trustedHeaderHashFromContract {
+		trustedHeaderHash, err = s.QueryBlockHeightToHeaderHashQueryName(s.Ctx(), start)
+		s.Require().NoError(err)
+	} else {
+		startBlock, err := s.GetBlockByHeight(ctx, int64(end))
+		s.Require().NoError(err)
+		trustedHeaderHash = common.BytesToHash(startBlock.LastCommit.BlockID.Hash)
+	}
+
+	endBlock, err := s.GetBlockByHeight(ctx, int64(end))
+	s.Require().NoError(err)
+	targetHeaderHash := common.BytesToHash(endBlock.LastCommit.BlockID.Hash)
+
+	pv := &publicValues{
+		TrustedBlock:      start,
+		TrustedHeaderHash: trustedHeaderHash,
+		TargetBlock:       end,
+		TargetHeaderHash:  targetHeaderHash,
+		BridgeCommitment:  bridgeCommitmentHash,
+	}
+	bz, err := pv.paddedBytes()
+	s.Require().NoError(err)
+
+	// Note: the mock SP1 verifier expects an empty proof. This is why we return nil.
+	return nil, bz
 }
 
 func (s *E2ETestSuite) GetDataForBridgeCommitmentInclusionProof(
