e02e86dda3fad445d8d199e025394e384b55a355
dillu24 <dylangalea96@gmail.com>
2025-03-14 10:59:42 +0100

    Removal of unnecessary parameters - MaxAuthorizedMessages, InjectedEventTxMaxBytes and AuthorizedMessagesAllowed (#476)
    Signed-off-by: Miguel Dingli <migueldingli97@gmail.com>
    Co-authored-by: Miguel Dingli <migueldingli97@gmail.com>

--- x/bridge/types/message_skip_event_tx.go
+++ x/bridge/types/message_skip_event_tx.go
@@ -8,7 +8,14 @@ import (
 
 var _ sdk.Msg = &MsgSkippedEventTx{}
 
-func NewMsgSkippedEventTx(authority string, reasonForSkip string, ethBlockNumber uint64, ethLogIndex uint64, ethTxIndex uint64, ethTxHash string) *MsgSkippedEventTx {
+func NewMsgSkippedEventTx(
+	authority string,
+	reasonForSkip string,
+	ethBlockNumber uint64,
+	ethLogIndex uint64,
+	ethTxIndex uint64,
+	ethTxHash string,
+) *MsgSkippedEventTx {
 	return &MsgSkippedEventTx{
 		Authority:      authority,
 		ReasonForSkip:  reasonForSkip,
