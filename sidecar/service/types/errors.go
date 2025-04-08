d38c0bf51d47999296cb2eea3eae5ab07accf663
Tharen Abela <tharen@simplystaking.com>
2025-04-08 13:16:56 +0400

    fixup! feat(app): fix authorization and error handling

--- sidecar/service/types/errors.go
+++ sidecar/service/types/errors.go
@@ -3,22 +3,8 @@ package types
 import "fmt"
 
 const (
-	AuthorizeEventTooManyMessagesStr       = "authorize event has too many messages"
 	GeneratedRawTxBytesExceededMaxBytesStr = "generated raw tx bytes exceeded max bytes"
 )
-
-type AuthorizeEventTooManyMessagesError struct {
-	actual   uint64
-	expected uint64
-}
-
-func (e *AuthorizeEventTooManyMessagesError) Error() string {
-	return fmt.Sprintf("%s; %d > %d", AuthorizeEventTooManyMessagesStr, e.actual, e.expected)
-}
-
-func NewAuthorizeEventTooManyMessagesError(actual, expected uint64) error {
-	return &AuthorizeEventTooManyMessagesError{actual: actual, expected: expected}
-}
 
 type GeneratedRawTxBytesExceededMaxBytesError struct {
 	txSize   uint64
