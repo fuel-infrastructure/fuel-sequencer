3bf7ecd279b20ae98ad94fc8df45ca81350e7f4f
Tharen Abela <tharen@simplystaking.com>
2025-04-14 16:27:54 +0400

    fixup! feat(bond): scaffold for bond module

--- x/bond/types/codec.go
+++ x/bond/types/codec.go
@@ -1,11 +1,9 @@
 package types
 
 import (
-    
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
-
 	// this line is used by starport scaffolding # 1
 )
 
@@ -14,8 +12,7 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgUpdateParams{},
+		&MsgBurnCoins{},
 	)
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
-
-
