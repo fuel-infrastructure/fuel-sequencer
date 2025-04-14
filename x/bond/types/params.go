b59a3d4e1c3cb151b047c27058f80d8778805599
Tharen Abela <tharen@simplystaking.com>
2025-04-14 15:12:35 +0400

    fixup! refactor(bond): update ParamSetPairs implementation

--- x/bond/types/params.go
+++ x/bond/types/params.go
@@ -93,37 +93,3 @@ func ValidateAuthority(i interface{}) error {
 
 	return nil
 }
-
-func validateInflation(i interface{}) error {
-	v, ok := i.(sdkmath.LegacyDec)
-	if !ok {
-		return fmt.Errorf("invalid parameter type: %T", i)
-	}
-
-	if v.IsNegative() {
-		return fmt.Errorf("inflation cannot be negative: %s", v)
-	}
-	if v.GT(sdkmath.LegacyOneDec()) {
-		return fmt.Errorf("inflation cannot be greater than 1: %s", v)
-	}
-
-	return nil
-}
-
-func validateAuthority(i interface{}) error {
-	v, ok := i.(string)
-	if !ok {
-		return fmt.Errorf("invalid parameter type: %T", i)
-	}
-
-	if strings.TrimSpace(v) == "" {
-		return nil // Empty string is allowed (will be set to governance module account)
-	}
-
-	_, err := sdk.AccAddressFromBech32(v)
-	if err != nil {
-		return fmt.Errorf("invalid authority address: %w", err)
-	}
-
-	return nil
-}
