92785dd910e595da0b13721c3f5bbda87dcdfee6
Tharen Abela <tharen@simplystaking.com>
2025-04-14 15:05:19 +0400

    refactor(bond): simplified error messages
    - Updated ErrInvalidSigner message for clarity.
    - Removed unused ErrSample definition.

--- x/bond/types/errors.go
+++ x/bond/types/errors.go
@@ -8,7 +8,5 @@ import (
 
 // x/bond module sentinel errors
 var (
-	ErrInvalidSigner = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
-    ErrSample        = sdkerrors.Register(ModuleName, 1101, "sample error")
-	
+	ErrInvalidSigner = sdkerrors.Register(ModuleName, 1100, "invalid signer")
 )
