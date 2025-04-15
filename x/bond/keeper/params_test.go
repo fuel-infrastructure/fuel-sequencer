712974ff4bb63e56ca8b9a6c4964905a50c420a6
Tharen Abela <tharen@simplystaking.com>
2025-04-15 19:53:58 +0400

    test(bond): verify params with non-default values
    - Add explicit checks to ensure test values differ from defaults before testing parameter setting

--- x/bond/keeper/params_test.go
+++ x/bond/keeper/params_test.go
@@ -3,16 +3,34 @@ package keeper_test
 import (
 	"testing"
 
+	sdkmath "cosmossdk.io/math"
 	"github.com/stretchr/testify/require"
 
-    keepertest "github.com/fuel-infrastructure/fuel-sequencer/testutil/keeper"
-    "github.com/fuel-infrastructure/fuel-sequencer/x/bond/types"
+	keepertest "github.com/fuel-infrastructure/fuel-sequencer/testutil/keeper"
+	"github.com/fuel-infrastructure/fuel-sequencer/x/bond/types"
 )
 
-func TestGetParams(t *testing.T) {
+func TestSetGetParams(t *testing.T) {
 	k, ctx := keepertest.BondKeeper(t)
+
+	// First check default values
+	defaultParams := types.DefaultParams()
+	require.EqualValues(t, sdkmath.LegacyZeroDec(), defaultParams.Inflation)
+	require.EqualValues(t, "", defaultParams.Authority)
+
+	// Set non-default values
 	params := types.DefaultParams()
+	params.Inflation = sdkmath.LegacyNewDecWithPrec(5, 1) // 0.5
+	params.Authority = "cosmos1qypqxpq9qcrsszg2pvxq6rs0zqg3yyc5lzv7xu"
+
+	// Verify non-default values are different from default
+	require.NotEqual(t, defaultParams.Inflation, params.Inflation)
+	require.NotEqual(t, defaultParams.Authority, params.Authority)
 
 	require.NoError(t, k.SetParams(ctx, params))
-	require.EqualValues(t, params, k.GetParams(ctx))
+
+	// Verify non-default values were set correctly
+	retrievedParams := k.GetParams(ctx)
+	require.EqualValues(t, sdkmath.LegacyNewDecWithPrec(5, 1), retrievedParams.Inflation)
+	require.EqualValues(t, "cosmos1qypqxpq9qcrsszg2pvxq6rs0zqg3yyc5lzv7xu", retrievedParams.Authority)
 }
