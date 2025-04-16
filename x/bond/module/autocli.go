553309e8898abbc9de937a543d91b27368459116
Tharen Abela <tharen@simplystaking.com>
2025-04-16 15:25:53 +0400

    refactor(bond): varargs for BurnCoins AutoCLI
    - BurnCoins cli allows multiple coin input.
    - Also minor formatting

--- x/bond/module/autocli.go
+++ x/bond/module/autocli.go
@@ -29,11 +29,13 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					Skip:      true, // skipped because authority gated
 				},
 				{
-					RpcMethod:      "BurnCoins",
-					Use:            "burn-coins [amount]",
-					Short:          "Burn coins from the sender's account",
-					Long:           "Burn coins from the sender's account. The amount must be a valid coin amount (e.g. 100fuel).",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "coins"}},
+					RpcMethod: "BurnCoins",
+					Use:       "burn-coins [amount]",
+					Short:     "Burn coins from the sender's account",
+					Long:      "Burn coins from the sender's account. The amount must be a valid coin amount (e.g. 100fuel).",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
+						{ProtoField: "coins", Varargs: true},
+					},
 				},
 				// this line is used by ignite scaffolding # autocli/tx
 			},
