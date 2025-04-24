2973cd656e5377344ebf366de1e2c6d8af0d725a
Tharen Abela <tharen@simplystaking.com>
2025-04-24 21:03:15 +0400

    fixup! feat(bond): introduce bond module upgrade handler and script

--- app/upgrades/bond_module/upgrade.go
+++ app/upgrades/bond_module/upgrade.go
@@ -14,11 +14,7 @@ func CreateUpgradeHandler(
 	configurator module.Configurator,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		// The bond module is already registered in the module manager through app wiring
-		// We just need to set its initial version in the version map
-		fromVM["bond"] = 1 // Initial version of the bond module
-
-		// Run any migrations
+		// Run migrations
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
 }
