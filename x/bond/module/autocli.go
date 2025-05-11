3facb4e63b53d9151bf9d51a76f7988b0add4b9e
Tharen Abela <tharen@simplystaking.com>
2025-05-11 20:53:53 +0400

    fixup! refactor(bond): remove per-block inflation and burn functionality

--- x/bond/module/autocli.go
+++ x/bond/module/autocli.go
@@ -28,15 +28,6 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					RpcMethod: "UpdateParams",
 					Skip:      true, // skipped because authority gated
 				},
-				{
-					RpcMethod: "BurnCoins",
-					Use:       "burn-coins [amount]",
-					Short:     "Burn coins from the sender's account",
-					Long:      "Burn coins from the sender's account. The amount must be a valid coin amount (e.g. 100fuel).",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
-						{ProtoField: "coins", Varargs: true},
-					},
-				},
 				// this line is used by ignite scaffolding # autocli/tx
 			},
 		},
