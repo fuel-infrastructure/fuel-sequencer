72e0f7b1fa4eb3dd54181611e3d84b3e74d27c6a
Tharen Abela <tharen@simplystaking.com>
2025-06-02 13:16:58 +0200

    fix(tests): DefaultBridgeDenom instead of "ufuel"

--- x/bridge/types/params_test.go
+++ x/bridge/types/params_test.go
@@ -105,7 +105,7 @@ func TestValidateBridgeDenom(t *testing.T) {
 		input     interface{}
 		expectErr bool
 	}{
-		{"Valid denom", "ufuel", false},
+		{"Valid denom", types.DefaultBridgeDenom, false},
 		{"Empty denom", "", true},
 		{"Non-string denom", 123, true},
 	}
@@ -324,7 +324,7 @@ func TestValidateSequencerTxsAllocation(t *testing.T) {
 }
 
 func TestParams_Validate(t *testing.T) {
-	validBridgeDenom := "ufuel"
+	validBridgeDenom := types.DefaultBridgeDenom
 	validBridgeDenomTotalSupply := sdkmath.NewInt(10_000_000_000)
 	validEthereumProxyContractAddress := "0x0165878A594ca255338adfa4d48449f69242Eb8F"
 	validSupplyDeltaPeriod := uint64(10)
