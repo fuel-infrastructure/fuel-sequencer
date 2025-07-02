c0032e4072f3f45ff578271fe3d7fbf596c1e0a1
Tharen Abela <tharen@simplystaking.com>
2025-07-02 09:32:34 +0200

    feat(upgrades): implement multi-vesting accounts upgrade handler
    - Replaced the existing features and optimisations upgrade handler with a new handler for multi-vesting accounts.
    - Added a new upgrade package to manage the multi-vesting accounts upgrade logic.

--- app/app.go
+++ app/app.go
@@ -56,7 +56,7 @@ import (
 	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	"github.com/fuel-infrastructure/fuel-sequencer/app/abci"
 	appcodec "github.com/fuel-infrastructure/fuel-sequencer/app/codec"
-	"github.com/fuel-infrastructure/fuel-sequencer/app/upgrades/features_and_optimisations"
+	"github.com/fuel-infrastructure/fuel-sequencer/app/upgrades/multi_vesting_accounts"
 	sidecarclient "github.com/fuel-infrastructure/fuel-sequencer/sidecar/client"
 	sidecarconfig "github.com/fuel-infrastructure/fuel-sequencer/sidecar/config"
 	commitmentsconfig "github.com/fuel-infrastructure/fuel-sequencer/x/commitments/config"
@@ -337,8 +337,8 @@ func NewFuelSequencerApp(
 	}
 
 	app.UpgradeKeeper.SetUpgradeHandler(
-		features_and_optimisations.UpgradeName,
-		features_and_optimisations.CreateUpgradeHandler(app.ModuleManager, app.Configurator()),
+		multi_vesting_accounts.UpgradeName,
+		multi_vesting_accounts.CreateUpgradeHandler(app.ModuleManager, app.Configurator()),
 	)
 
 	// PREPARE AND PROCESS PROPOSAL HANDLERS
