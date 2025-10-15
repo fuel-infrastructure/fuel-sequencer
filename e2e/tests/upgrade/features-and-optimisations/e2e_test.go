40144e078355b348fb8caf0325483ea8087b0f3f
Tharen Abela <-h>
2025-10-15 17:04:45 +0200

    chore: e2e auto lint fixes applied

--- e2e/tests/upgrade/features-and-optimisations/e2e_test.go
+++ e2e/tests/upgrade/features-and-optimisations/e2e_test.go
@@ -128,7 +128,7 @@ func (s *UpgradesTestSuite) TestUpgrade() {
 		from := s.EthKeys[0]
 		to := s.EthKeys[1]
 		transfer := testsuite.PackTransfer(to.Address, big.NewInt(sendAmount))
-		msgSendBz := s.E2ETestSuite.GenerateMsgSendBz(
+		msgSendBz := s.GenerateMsgSendBz(
 			from.AddressHex, to.AddressHex,
 			sdk.NewCoins(sdk.NewCoin(testsuite.BridgeDenom, sdkmath.NewInt(sendAmount))),
 		)
