126c487c6a8c202092b634654eb53195891018c6
Tharen Abela <-h>
2025-12-16 02:30:36 +0100

    feat(e2e/cluster): execute with output capture
    - Updated the `execute` function to return captured stdout output along with error handling.
    - Introduced a `silent` parameter to control logging behavior for stdout and stderr.
    - Added `LocallyWithOutput` and `RemotelyWithOutput` functions to facilitate output capture in local and remote command executions.

--- e2e/cluster/pkg/execute/remote.go
+++ e2e/cluster/pkg/execute/remote.go
@@ -23,5 +23,17 @@ func RemotelyWithCustomName(l *zap.SugaredLogger, client *ssh.Client, cmd, cmdna
 	}
 	defer session.Close()
 
-	return execute(l.Named("SSH"), &sshSessionExecutor{Session: session, cmd: cmd}, cmdname)
+	_, err = execute(l.Named("SSH"), &sshSessionExecutor{Session: session, cmd: cmd}, cmdname, false)
+	return err
+}
+
+func RemotelyWithOutput(l *zap.SugaredLogger, client *ssh.Client, cmd, cmdname string) (string, error) {
+	// Create new SSH client
+	session, err := client.NewSession()
+	if err != nil {
+		return "", fmt.Errorf("failed to create session: %w", err)
+	}
+	defer session.Close()
+
+	return execute(l.Named("SSH"), &sshSessionExecutor{Session: session, cmd: cmd}, cmdname, false)
 }
